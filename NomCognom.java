0f9d7928738b27c3ac04769d1fbd8a758426b2a8
Javier Garcia Vera <cf22javier.garcia@iesjoandaustria.org>
2022-09-21 19:11:32 +0200

    excercici nomcognom

---
+++ NomCognom.java
@@ -0,0 +1,13 @@
+public class NomCognom {
+    public static void main(String[] args)
+     {
+  
+        System.out.println("Nom?");
+        String nom = Entrada.readLine();
+        
+        System.out.println("Cognom?");
+        String cognom = Entrada.readLine();
+        
+        System.out.println("Ei " + nom + ",tinc una amiga que també és " + cognom);
+        }
+     }
