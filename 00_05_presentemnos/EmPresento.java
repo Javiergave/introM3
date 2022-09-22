2726a85194703fd0a51697712f19adaf3b7fb82e
Javier Garcia Vera <cf22javier.garcia@iesjoandaustria.org>
2022-09-22 19:51:01 +0200

    Completat l'exercici en que em presento

---
+++ 00_05_presentemnos/EmPresento.java
@@ -0,0 +1,6 @@
+/* Programa que saluda al món */
+public class EmPresento {
+    public static void main(String[] args) {
+        System.out.println("Hola, sóc Javier, el futur millor desenvolupador del Món");
+    }
+}
