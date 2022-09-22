38933e8768f5cf02c98733555a19d39724ee605b
prgtest <cf22javier.garcia@iesjoandaustria.org>
2022-09-22 16:22:44 +0200

    creada carpeta de ejercicio nomcognom  ∾ prgtest autocommit 31_05 #4

--- 31_05_nomcognom/NomCognom.java
+++ 31_05_nomcognom/NomCognom.java
@@ -5,7 +5,7 @@ public class NomCognom {
         System.out.println("Nom?");
         String nom = Entrada.readLine();
         
-        System.out.println("Primer Cognom?");
+        System.out.println("Primer cognom?");
         String cognom = Entrada.readLine();
         
         System.out.println("Ei " + nom + ", tinc una amiga que també és " + cognom + ".");
