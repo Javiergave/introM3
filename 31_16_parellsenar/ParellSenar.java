13ac919b8d58ce552edf1571c3be2391f3b6518a
Javier Garcia Vera <cf22javier.garcia@iesjoandaustria.org>
2022-09-29 20:07:35 +0200

    parellsenar

--- 31_16_parellsenar/ParellSenar.java
+++ 31_16_parellsenar/ParellSenar.java
@@ -7,10 +7,10 @@ public class ParellSenar {
     public static void main(String[] args) {
         int numero = Integer.parseInt(args[0]);
         if ((numero%2)!=0){
-        System.out.println("El número "+numero+" es senar");
+        System.out.println("El número "+numero+" és senar");
         }
         else {
-        System.out.println("El número "+numero+" es parell");
+        System.out.println("El número "+numero+" és parell");
         }
     }
 }
