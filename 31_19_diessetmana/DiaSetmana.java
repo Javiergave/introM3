4a01a34187436ac49f6a67b68f2a72e3360ab88d
Javier Garcia Vera <cf22javier.garcia@iesjoandaustria.org>
2022-09-30 17:23:49 +0200

    Dias setmana

---
+++ 31_19_diessetmana/DiaSetmana.java
@@ -0,0 +1,30 @@
+public class DiaSetmana {
+  public static void main(String[] args) {
+   int dia = Integer.parseInt(args[0]);
+   if (dia>=1 && dia<=7){
+     if (dia==1){
+     System.out.println("Dilluns");
+     }
+        else if (dia==2){
+         System.out.println("Dimarts");
+         }
+            else if (dia==3){
+            System.out.println("Dimecres");
+            }
+                else if (dia==4){
+                     System.out.println("Dijous");
+                     }
+                        else if (dia==5){
+                            System.out.println("Divendres");
+                            }
+                                else if (dia==6){
+                                    System.out.println("Dissabte");
+                                    }
+                                        else if (dia==7){
+                                            System.out.println("Diumenge");
+                                            }
+}
+   else { System.out.println("Error");}
+
+}}
+                           
