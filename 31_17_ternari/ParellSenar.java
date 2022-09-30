5e09dba771977855f9def11b2bbfbbdfce2256c9
Javier Garcia Vera <cf22javier.garcia@iesjoandaustria.org>
2022-09-30 15:54:10 +0200

    test ternari

--- 31_17_ternari/ParellSenar.java
+++ 31_17_ternari/ParellSenar.java
@@ -3,12 +3,12 @@
  * és parell o senar.
  * Si no s'especifica cap número, el resultat és indeterminat.
  */
-public class Ternari {
+public class ParellSenar {
     public static void main(String[] args) {
         int numero = Integer.parseInt(args[0]);
-        int resultat = (numero%2)!=0;
-        int resultat = condicio ? 1 : 2;
+        String resultat = (numero%2)!=0 ? "senar" : "parell";
         System.out.println("El número "+numero+" és "+resultat);
+       
     }
 }
 
