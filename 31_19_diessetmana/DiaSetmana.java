61942d109de67b923f7af3141a708132b4b34c5f
Javier Garcia Vera <cf22javier.garcia@iesjoandaustria.org>
2022-10-04 14:53:00 +0200

    capçalera diasetmana

--- 31_19_diessetmana/DiaSetmana.java
+++ 31_19_diessetmana/DiaSetmana.java
@@ -1,3 +1,11 @@
+/*
+ * Aquest programa agafa un nombre de l'1 al 7 i diu el dia de la setmana que li correspon
+ *
+ * Demana un nombres per entrada estàndard i mostra el
+ * dia de la setmana corresponent a aquest número
+ *
+ * En cas que el nombre introduit no sigui de l'1 al 7 es mostrarà un missatge de "Error"
+ */
 public class DiaSetmana {
   public static void main(String[] args) {
    int dia = Integer.parseInt(args[0]);
@@ -23,7 +31,7 @@ public class DiaSetmana {
                                         else if (dia==7){
                                             System.out.println("Diumenge");
                                             }
-}
+   }
    else { System.out.println("Error");}
 
 }}
