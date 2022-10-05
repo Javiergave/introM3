f8c497416be08ea0a8dafce5697ae4f24a747e40
Javier Garcia Vera <cf22javier.garcia@iesjoandaustria.org>
2022-10-05 17:17:27 +0200

    31-22

--- 31_22_mesosanys/MesosAnys.java
+++ 31_22_mesosanys/MesosAnys.java
@@ -6,10 +6,17 @@ System.out.println("Mes?");
 int mes = Integer.parseInt(Entrada.readLine());
 System.out.println("Any?");
 int any = Integer.parseInt(Entrada.readLine());
-if (mes + 1==13){
-    mes=1;
-    }
+switch (mes){
+case 2,3,4,5,6,7,8,9,10,11:
 System.out.println("Mes anterior "+(mes-1)+"/"+any+" i mes següent "+(mes+1)+"/"+any);
+break;
+case 1:
+System.out.println("Mes anterior "+(mes+11)+"/"+(any-1)+" i mes següent "+(mes+1)+"/"+any);
+break;
+case 12:
+System.out.println("Mes anterior "+(mes-1)+"/"+any+" i mes següent "+(mes-11)+"/"+(any+1));
+break;
+}
 }
 }
 
