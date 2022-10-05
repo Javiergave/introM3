49eb98e8776fb373af8664b9b0b3e704316f065c
Javier Garcia Vera <cf22javier.garcia@iesjoandaustria.org>
2022-10-05 19:14:42 +0200

    31_24

--- 31_24_calculadora_senzilla/CalculadoraSenzilla.java
+++ 31_24_calculadora_senzilla/CalculadoraSenzilla.java
@@ -14,6 +14,9 @@ public class CalculadoraSenzilla {
         double operand2 =Double.parseDouble(Entrada.readLine());
         
         // operacio
+        if ((operador=='/')&&(operand2=='0')){
+        System.out.println("No es pot dividir entre 0");
+        }
        if (operador == '+') {
             double resultat = operand1 + operand2;
             System.out.println(operand1 + " + " + operand2 + " = " + resultat);
@@ -25,8 +28,6 @@ public class CalculadoraSenzilla {
             System.out.println(operand1 + " * " + operand2 + " = " + resultat);
         } else if (operador == '/') {
          double resultat = operand1 / operand2;
-         if (operand2 == 0){
-         System.out.println("No es pot dividir entre 0");}
             System.out.println(operand1 + " / " + operand2 + " = " + resultat);
         } else if (operador == '%') {
          double resultat = operand1 % operand2;
