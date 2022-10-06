283537cf4484c0225929e4fa61a34faa0a37afb6
Javier Garcia Vera <cf22javier.garcia@iesjoandaustria.org>
2022-10-06 16:34:38 +0200

    31_25

---
+++ 31_25_pagaments/Pagaments.java
@@ -0,0 +1,18 @@
+//Aquest programa simula una transacció amb la quantitat a pagar i la pagada, de manera que el programa indiqui si sobren o falten diners.//
+public class Pagaments{
+public static void main(String[] args) {
+    System.out.println("Preu?");
+        int preu = Integer.parseInt(Entrada.readLine());
+    System.out.println("Paga?");
+        int paga = Integer.parseInt(Entrada.readLine());
+if (paga>preu){
+int sobra = paga - preu;
+ System.out.println("Sobren "+sobra+"€");
+} else if (paga<preu){
+int falta = preu-paga;
+ System.out.println("Falten "+falta+"€");
+ } else {
+ System.out.println("No sobra ni falta res");
+ }
+}
+}
