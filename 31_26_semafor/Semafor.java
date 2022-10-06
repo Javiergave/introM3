3ffe331f71346c8121ed3f5ec069075cc3bca1cb
Javier Garcia Vera <cf22javier.garcia@iesjoandaustria.org>
2022-10-06 16:43:00 +0200

    semafor

---
+++ 31_26_semafor/Semafor.java
@@ -0,0 +1,16 @@
+// Aquest programa rep el color del semafor i et diu que fer 
+public class Semafor {
+    public static void main(String[] args) {
+        System.out.println("Color?");
+        String color = Entrada.readLine();
+        if (color.equals("groc")) {
+          System.out.println("corre!");
+        } else if (color.equals("verd")) {
+            System.out.println("passa");
+        } else if (color.equals("vermell")) {
+            System.out.println("espera");
+        } else {
+        System.out.println("ves a l'oculista");
+        }
+}
+}
