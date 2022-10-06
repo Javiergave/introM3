a529d49e17e818e21daaa5c0039c6eb762d1b79a
Javier Garcia Vera <cf22javier.garcia@iesjoandaustria.org>
2022-10-06 19:50:51 +0200

    prgtest ascensor

---
+++ 31_28_ascensor/Ascensor.java
@@ -0,0 +1,42 @@
+// Aquest programa simula un ascensor
+public class Ascensor {
+    public static void main(String[] args) {
+        System.out.println("pis?");
+        String pis = Entrada.readLine();
+        System.out.println("botó?");
+        String boto = Entrada.readLine();
+        if (((pis.equals("planta baixa"))||(pis.equals("primera planta"))||(pis.equals("segona planta")))&&((boto.equals("pujar un"))||(boto.equals("pujar dos"))||(boto.equals("baixar un"))||(boto.equals("baixar dos")))){
+            if ((pis.equals("planta baixa"))&&((boto.equals("baixar un"))||((boto.equals("baixar dos"))))){
+            System.out.println("error");
+            }
+            if ((pis.equals("segon pis"))&&((boto.equals("pujar un"))||((boto.equals("pujar dos"))))){
+            System.out.println("error");
+            }
+            if ((pis.equals("primer pis"))&&((boto.equals("baixar dos"))||((boto.equals("pujar dos"))))){
+            System.out.println("error");
+            }
+            if (pis.equals("planta baixa")&&(boto.equals("pujar un"))){
+                System.out.println("primer pis");
+            }   
+            if (pis.equals("planta baixa")&&(boto.equals("pujar dos"))){
+                System.out.println("segon pis");
+            }
+            if (pis.equals("primera planta")&&(boto.equals("pujar un"))){
+                System.out.println("segon pis");
+            }
+            if (pis.equals("primera planta")&&(boto.equals("baixar un"))){
+                System.out.println("planta baixa");
+            }
+            if (pis.equals("segona planta")&&(boto.equals("baixar dos"))){
+                System.out.println("planta baixa");
+            }
+            if (pis.equals("segona planta")&&(boto.equals("baixar un"))){
+                System.out.println("primera planta");
+            }
+     
+        } 
+   else {
+    System.out.println("error");
+    }
+}
+}
