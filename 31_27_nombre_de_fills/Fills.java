34239794097809117531cb015e809e45e829940f
prgtest <cf22javier.garcia@iesjoandaustria.org>
2022-10-11 15:36:39 +0200

    compilat 31 27  ∾ prgtest autocommit 31_27 #4

--- 31_27_nombre_de_fills/Fills.java
+++ 31_27_nombre_de_fills/Fills.java
@@ -3,10 +3,11 @@ public class Fills {
     public static void main(String[] args) {
         System.out.println("Quants fills tens?");
         int fills = Integer.parseInt(Entrada.readLine());
-        if (fills>=0){
-        System.out.println("Tot el que t'has estalviat en bolquers!");
-            if (fills==1){
-            System.out.println(" Compte de no mimar-lo massa!");
+        if (fills>0){
+            if (fills==0){
+            System.out.println("Tot el que t'has estalviat en bolquers!");
+            } else if (fills==1){
+            System.out.println("Compte de no mimar-lo massa!");
             }
             else if ((fills>=2)&&(fills<5)){
             System.out.println("No t'avorreixes a casa, eh?");
