50d7c40e32dad61da957358dc42c4886733b5393
prgtest <cf22javier.garcia@iesjoandaustria.org>
2022-10-13 19:50:41 +0200

    31_34  ∾ prgtest autocommit 31_34 #2

--- 31_34_lloro/Lloro.java
+++ 31_34_lloro/Lloro.java
@@ -1,18 +1,18 @@
+//aquest programa repeteix els inputs fins que no rebi un
 public class Lloro {
     public static void main(String[] args) {
 
         System.out.println("El lloro espera paraula:");
         String paraula = Entrada.readLine();;
-        while (!(paraula.isEmpty())) {
+        if (!(paraula.isEmpty())) {
             
             System.out.println("El lloro repeteix: " + paraula);
             System.out.println("El lloro espera paraula:");
             paraula = Entrada.readLine();;
             
-       }    
-                
-
-        // mostrem el resultat
+       } else {    
+    
         System.out.println("Adéu");
     }
+}
 }
