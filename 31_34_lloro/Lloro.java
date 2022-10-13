25c50c6464ffcdd3c50f4440db988aaf925b2056
Javier Garcia Vera <cf22javier.garcia@iesjoandaustria.org>
2022-10-13 19:47:45 +0200

    31_34

---
+++ 31_34_lloro/Lloro.java
@@ -0,0 +1,18 @@
+public class Lloro {
+    public static void main(String[] args) {
+
+        System.out.println("El lloro espera paraula:");
+        String paraula = Entrada.readLine();;
+        while (!(paraula.isEmpty())) {
+            
+            System.out.println(paraula);
+            System.out.println("El lloro espera paraula:");
+            paraula = Entrada.readLine();;
+            
+       }    
+                
+
+        // mostrem el resultat
+        System.out.println("Adéu");
+    }
+}
