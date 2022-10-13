fcff10fcfa23a52960d774dddc83eda0062bb158
Javier Garcia Vera <cf22javier.garcia@iesjoandaustria.org>
2022-10-13 19:31:48 +0200

    31 32

---
+++ 31_33_quantsparells/QuantsParells.java
@@ -0,0 +1,29 @@
+public class QuantsParells {
+    public static void main(String[] args) {
+
+        // creem la variable on guardarem el resultat de sumar
+        int parells = 0;   // inicialment no hem sumat res i per tant és 0
+
+        // demanem valors
+
+        // declarem la variable que contindrà els valors llegits
+        int valor;
+
+        // recollim primera nota
+        System.out.println("Introdueix un valor");
+        valor = Integer.parseInt(Entrada.readLine());
+        while (valor >= 0) {
+            if ( valor % 2 == 0){
+            parells = parells + 1;
+            }
+            System.out.println("Introdueix un valor");
+            valor = Integer.parseInt(Entrada.readLine());
+            
+            
+       }    
+                
+
+        // mostrem el resultat
+        System.out.println("El nombre de parells introduïts és " + parells);
+    }
+}
