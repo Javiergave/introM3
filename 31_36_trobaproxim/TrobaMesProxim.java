521a2828d63d4cff8b3871b75424b4637e7ea186
prgtest <cf22javier.garcia@iesjoandaustria.org>
2022-10-18 18:17:41 +0200

    31_40  ∾ prgtest autocommit 31_36 #1

--- 31_36_trobaproxim/TrobaMesProxim.java
+++ 31_36_trobaproxim/TrobaMesProxim.java
@@ -2,49 +2,61 @@
 public class TrobaMesProxim {
     public static void main(String[] args) {
 
-        int distanciaAnt = -1;
-        int valorProxim = -1;
+        int distanciaAnt = 345676543;
+        int distancia;
         
         System.out.println("Introdueix l'àncora");
         int ancora = Integer.parseInt(Entrada.readLine());
+        
         if (ancora >= 0) {
-        System.out.println("Introdueix un valor");
-        int valor = Integer.parseInt(Entrada.readLine());
-            if (valor>=0){
-                if (valor >= ancora){
-                    distanciaAnt = valor-ancora;    
-                } else {
-                    distanciaAnt = ancora-valor;
-                }
-            } else { 
+        
+            System.out.println("Introdueix un valor");
+            int valor = Integer.parseInt(Entrada.readLine());
+            int valorProxim = valor;
+                if (valor>=0){
+            
+                    if (valor > ancora){
+                        distanciaAnt = valor-ancora; 
+                    }
+                 
+                    else {
+                        distanciaAnt = ancora-valor;
+                    }
+                
+                } 
+            
+                else { 
                 System.out.println("No s'ha introduït cap valor positiu");
                 }
             
             
             while (valor >= 0){
-            System.out.println("Introdueix un valor");
-            valor = Integer.parseInt(Entrada.readLine());
             
-                if (valor >= ancora){
-                int distancia = valor-ancora;
+                System.out.println("Introdueix un valor");
+                valor = Integer.parseInt(Entrada.readLine());
                 
-                    if (distancia<distanciaAnt) {
-                        if (valorProxim>valor){
-                            valorProxim = valor;
-                        }
-                    }
-                } else {
-                    int distancia = ancora-valor;
-                    if (distancia <= distanciaAnt) {
-                        if (valorProxim>valor){
-                            valorProxim = valor;
-                        }
-                    }
+                if (valor >= ancora){
+                    distancia = valor-ancora; 
                 }
-         
-       } 
-       System.out.println("El nombre més pròxim a "+ancora+" és "+valorProxim);
-       }else {   
+                 
+                else {
+                   distancia = ancora-valor;
+                }
+                
+                if (distancia<distanciaAnt){
+                    valorProxim = valor;
+                }
+                
+                if ((distancia<distanciaAnt)&&(valor<valorProxim)){
+                    valorProxim = valor;
+                }
+
+            }
+
+            System.out.println("El nombre més pròxim a "+ancora+" és "+valorProxim);
+       }
+       
+       else {   
             System.out.println("Àncora no vàlida");
        }
     }
