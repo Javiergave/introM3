2444a3d4c2b18d22c270421bd90d967fdd1ffe4e
Javier Garcia Vera <cf22javier.garcia@iesjoandaustria.org>
2022-10-18 16:47:07 +0200

    31_39

--- 31_37_mitja/NotaMitja.java
+++ 31_37_mitja/NotaMitja.java
@@ -1,7 +1,7 @@
 public class NotaMitja {
     public static void main(String[] args) {
         
-        int notesIntroduides = 1
+        int notesIntroduides = 1;
         
         System.out.println("Introdueix un valor");
         int nota = Integer.parseInt(Entrada.readLine());
@@ -13,16 +13,16 @@ public class NotaMitja {
         while (nota >= 0){
         
             if (nota<100){
-            notesIntroduides = notesIntroduides + 1
+            notesIntroduides = notesIntroduides + 1;
             
-            int notaTotal = nota + notaTotal
+            int notaTotal = nota + notaTotal;
             }
             
             System.out.println("Introdueix un valor");
             valor = Integer.parseInt(Entrada.readLine());
         }
         
-       int mitja = notaTotal / notesIntroduides
+       int mitja = notaTotal / notesIntroduides;
         
         System.out.println("La mitja de les notes vàlides és "+mitja);
         
