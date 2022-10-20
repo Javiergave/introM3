da1d073c1e506f77ad5766d46f49e87cbc5d2b41
Javier Garcia Vera <cf22javier.garcia@iesjoandaustria.org>
2022-10-20 20:38:26 +0200

    31_44

---
+++ 31_44_de_principi_a_fi/DeuPrimers.java
@@ -0,0 +1,11 @@
+//Aquest excercici mostra els nombres de 1 a 10 amb un for
+public class DeuPrimers{
+    public static void main(String[] args) {
+
+        for (int n = 1; n <= 10; n = n + 1){
+            System.out.println(n);
+        }
+
+
+    }
+}
