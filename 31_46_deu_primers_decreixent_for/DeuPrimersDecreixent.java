35e01a64ff45c56d3b14d4c1594abab31d3c5c21
Javier Garcia Vera <cf22javier.garcia@iesjoandaustria.org>
2022-10-21 16:35:14 +0200

    31_46

---
+++ 31_46_deu_primers_decreixent_for/DeuPrimersDecreixent.java
@@ -0,0 +1,10 @@
+public class DeuPrimersDecreixent{ 
+    public static void main(String[] args) {  
+
+        for (int n = 10; n >= 1 ;n--){
+            System.out.println(n); 
+        }
+
+    }
+}
+        
