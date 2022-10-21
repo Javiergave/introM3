7047e53bd6de1af732908f9dc46ac1e322e8a91d
prgtest <cf22javier.garcia@iesjoandaustria.org>
2022-10-21 15:50:44 +0200

    31_45  ∾ prgtest autocommit 31_45 #1

--- 31_45_deu_primers_for/DeuPrimers.java
+++ 31_45_deu_primers_for/DeuPrimers.java
@@ -1,7 +1,7 @@
 public class DeuPrimers{
     public static void main(String[] args) {
         
-        for(int n = 1; n >=10; n++){
+        for(int n = 1; n < 10; n++){
             System.out.println(n);
         }
 
