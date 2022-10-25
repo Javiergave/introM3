45523613068047799227e439b03624805a2a9b65
Javier Garcia Vera <cf22javier.garcia@iesjoandaustria.org>
2022-10-25 18:41:35 +0200

    31_54

--- 31_54_triangle_invertit/TriangleInvertit.java
+++ 31_54_triangle_invertit/TriangleInvertit.java
@@ -1,23 +1,32 @@
+//Aquest programa et Desenvolupa un programa que demani un nombre positiu més gran que 0. El programa escriu una línia per nombre entre el nombre introduït i el 1 (és a dir, decreixentment). A cada línia escriu tots els nombres des de l’1 fins el nombre corresponent a la línia.
+
 public class TriangleInvertit {
     public static void main(String[] args) {     
         
         System.out.println("Nombre?");
         int valor = Integer.parseInt(Entrada.readLine()); 
         
-        for (int i = valor; i>=1; i--){
-            for (int j = 1; j<=i; j++){
+        if (valor > 0){
 
-                if (j!=i){
-                    System.out.print(j+", ");
-                }
+        
 
-                else {
-                    System.out.print(j); 
-                }
+            for (int i = valor; i>=1; i--){
+            
+                for (int j = 1; j<=i; j++){
+
+                    if (j!=i){
+                        System.out.print(j+", ");
+                    }
+
+                    else {
+                        System.out.print(j); 
+                    }
                 
-            }
+                }
 
-            System.out.println(); 
+                System.out.println(); 
+
+            }
 
         }
 
