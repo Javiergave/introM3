9bf3105e5d005b0dcb57c7d5a5eca8730240d090
Javier Garcia Vera <cf22javier.garcia@iesjoandaustria.org>
2022-10-25 18:33:33 +0200

    31_54

--- 31_54_triangle_invertit/TriangleInvertit.java
+++ 31_54_triangle_invertit/TriangleInvertit.java
@@ -6,15 +6,15 @@ public class TriangleInvertit {
         
         for (int i = valor; i>=1; i--){
             for (int j = 1; j<=i; j++){
+
                 if (j!=i){
                     System.out.print(j+", ");
                 }
+
                 else {
                     System.out.print(j); 
                 }
                 
-                
-
             }
 
             System.out.println(); 
