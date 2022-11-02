0abbde644c19ac0ff296c6ac0b20610bd1742a7b
prgtest <cf22javier.garcia@iesjoandaustria.org>
2022-11-02 16:37:29 +0100

    31_51  ∾ prgtest autocommit 31_51 #6

--- 31_51_historic_edats/HistoricEdats.java
+++ 31_51_historic_edats/HistoricEdats.java
@@ -1,3 +1,5 @@
+/*Aquest programa demana el nom d'una persona, la seva edat i l'any actual, 
+i escriu l'edat que tenia aquella persona cada any des del seu naixement. */
 public class HistoricEdats {
     public static void main(String[] args) {  
         
@@ -10,29 +12,36 @@ public class HistoricEdats {
         System.out.println("L'any actual?");
         int any_actual = Integer.parseInt(Entrada.readLine()); 
         
-        int any_neixement = any_actual - edat_actual;
-
         if (edat_actual>0){
 
-        System.out.println("El " +any_neixement+ " vau néixer");
+            int any_neixement = any_actual - edat_actual;
+
+            if (edat_actual>0){
+
+                System.out.println("El " +any_neixement+ " vau néixer");
         
-        }
+            }
 
-        int any = any_neixement + 1;
+            int any = any_neixement + 1;
 
-        if  (edat_actual > 1){
+            if  (edat_actual > 1){
 
-        System.out.println("El " +any+ " teníeu 1 any");
+                System.out.println("El " +any+ " teníeu 1 any");
         
+            }
+
+            any = any + 1;
+
+            for (int edat = 2; any < any_actual; any++, edat++ ){
+                System.out.println("El " +any+ " teníeu " +edat+ " anys"); 
+            }
+
+            System.out.println("Adéu " +paraula); 
         }
 
-        any = any + 1;
-
-        for (int edat = 2; any < any_actual; any++, edat++ ){
-            System.out.println("El " +any+ " teníeu " +edat+ " anys"); 
+        else{
+            System.out.println("Entrada incorrecta");
         }
-
-        System.out.println("Adéu " +paraula); 
 
     }
 }
