b46e836fb7bafdf68aab48d803f3c58b4a2fee84
prgtest <cf22javier.garcia@iesjoandaustria.org>
2022-11-02 16:55:07 +0100

    31_59  ∾ prgtest autocommit 31_59 #6

--- 31_59_taula_multiplicar/TaulaMultiplicar.java
+++ 31_59_taula_multiplicar/TaulaMultiplicar.java
@@ -7,13 +7,9 @@ public class TaulaMultiplicar {
         int valor3 = Integer.parseInt(args[2]);
         int valor4 = Integer.parseInt(args[3]);
 
-        for (int i = valor1, j = valor3; i <= valor2 && j<=valor4; i++, j++){
-            System.out.println(i+ " x " +j+ " = " +i*j);
-
-            j = j + 1;
-
-            if (valor1!= valor2){
-                System.out.println(i+ " x " +j+ " = " +i*j);  
+        for (int i = valor1; i <= valor2; i++){
+            for (int j = valor3; j<= valor4; j++){
+                System.out.println(i+ " x " +j+ " = " +i*j); 
             }
         }
 
