3438e8f121a6658742712e1753348d3fc2bbd85d
Javier Garcia Vera <cf22javier.garcia@iesjoandaustria.org>
2022-11-03 16:02:51 +0100

    31_61

---
+++ 31_61_informestring/InformeString.java
@@ -0,0 +1,29 @@
+/* Aquest programa demana dos texts i un enter positiu i composa un petit informe.*/
+
+public class InformeString { 
+    public static void main(String[] args) {  
+
+        System.out.println("Text principal?"); 
+        String paraula = Entrada.readLine(); 
+
+        System.out.println("Text secundari?");
+        String paraula2 = Entrada.readLine();  
+
+        System.out.println("Número positiu?"); 
+        int valor = Integer.parseInt(Entrada.readLine()); 
+
+        System.out.println("\""+paraula +"\".length(): "+paraula.length());
+        System.out.println("\""+paraula +"\".startsWith(\""+paraula2+"\"): "+paraula.startsWith(paraula2)); 
+        System.out.println("\""+paraula +"\".endsWith(\""+paraula2+"\"): "+paraula.endsWith(paraula2)); 
+        System.out.println("\""+paraula +"\".equals(\""+paraula2+"\"): "+paraula.equals(paraula2)); 
+        System.out.println("\""+paraula +"\".equalsIgnoreCase(\""+paraula2+"\"): "+paraula.equalsIgnoreCase(paraula2));
+        System.out.println("\""+paraula +"\".isBlank(): "+paraula.isBlank());
+        System.out.println("\""+paraula +"\".isEmpty(): "+paraula.isEmpty());
+        System.out.println("\""+paraula +"\".charAt("+valor+"): "+paraula.charAt(valor));
+        System.out.println("\""+paraula +"\".concat(\""+valor+"\"): "+paraula.concat(paraula2));
+        System.out.println("\""+paraula +"\".repeat("+valor+"): "+paraula.repeat(valor));
+        System.out.println("\""+paraula +"\".toLowerCase(): "+paraula.toLowerCase()); 
+        System.out.println("\""+paraula +"\".toUpperCase(): "+paraula.toUpperCase()); 
+
+    }
+}
