0c0287da2dcb0bd4245c424f5a14fba87c7ea448
Javier Garcia Vera <cf22javier.garcia@iesjoandaustria.org>
2022-11-08 18:28:50 +0100

    31_64

--- 31_64_inicial_vocal_minuscula/LloroVocalMinuscules.java
+++ 31_64_inicial_vocal_minuscula/LloroVocalMinuscules.java
@@ -1,20 +1,22 @@
+//Aquest programa nomes repeteix les paraules que comencin en vocal minuscula i s'atura quan l'input sigui buit
 public class LloroVocalMinuscules { 
     public static void main(String[] args) {  
 
         System.out.println("El lloro pregunta paraula que comenci amb vocal en minúscules");
         String paraula = Entrada.readLine();
         // A l'inici vaig probar amb un or de (paraula = paraula.trim();)
-
-            while (((paraula.isBlank())==false)) {
+        
+        while (((paraula.isBlank()))==false) {
                 
-                while (){
-                    
-                }
+            if (paraula.charAt(0)=='a'||paraula.charAt(0)=='e'||paraula.charAt(0)=='i'||paraula.charAt(0)=='o'||paraula.charAt(0)=='u'){
+                System.out.println(paraula);
+            }   
                 
-                System.out.println("El lloro espera paraula:");
-                paraula = Entrada.readLine();
-            }
-        System.out.println("Adéu")
+            System.out.println("El lloro pregunta paraula que comenci amb vocal en minúscules");
+            paraula = Entrada.readLine();
+            paraula = paraula.trim();
+        }
 
+        System.out.println("Adéu");
     }
 }
