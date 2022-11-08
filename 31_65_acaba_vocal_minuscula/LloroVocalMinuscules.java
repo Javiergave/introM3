86a8303b0eb0dbaf724e42c4fed5d97dd3550105
prgtest <cf22javier.garcia@iesjoandaustria.org>
2022-11-08 18:35:58 +0100

    31_65  ∾ prgtest autocommit 31_65 #3

--- 31_65_acaba_vocal_minuscula/LloroVocalMinuscules.java
+++ 31_65_acaba_vocal_minuscula/LloroVocalMinuscules.java
@@ -8,7 +8,7 @@ public class LloroVocalMinuscules {
         
         while (((paraula.isBlank()))==false) {
                 
-            if (paraula.charAt(paraula.length())=='a'||paraula.charAt(paraula.length())=='e'||paraula.charAt(paraula.length())=='i'||paraula.charAt(paraula.length())=='o'||paraula.charAt(paraula.length())=='u'){
+            if (paraula.charAt(paraula.length()-1)=='a'||paraula.charAt(paraula.length()-1)=='e'||paraula.charAt(paraula.length()-1)=='i'||paraula.charAt(paraula.length()-1)=='o'||paraula.charAt(paraula.length()-1)=='u'){
                 System.out.println("El lloro diu: "+paraula);
             }   
                 
