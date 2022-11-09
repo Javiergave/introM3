ef54fb056bc01365d3b3ecaea8a128a69de7c9c7
prgtest <cf22javier.garcia@iesjoandaustria.org>
2022-11-09 15:36:14 +0100

    31_66  ∾ prgtest autocommit 31_66 #6

--- 31_66_acaba_lletra/LloroAcabaLletra.java
+++ 31_66_acaba_lletra/LloroAcabaLletra.java
@@ -1,3 +1,5 @@
+import java.sql.Blob;
+
 //Aquest programa nomes repeteix les paraules que no acabin en vocal minuscula i s'atura quan l'input sigui buit
 public class LloroAcabaLletra { 
     public static void main(String[] args) {  
@@ -5,16 +7,27 @@ public class LloroAcabaLletra {
         System.out.println("El lloro pregunta paraula que finalitzi per lletra no vocal");
         String paraula = Entrada.readLine();
         // A l'inici vaig probar amb un or de (paraula = paraula.trim();)
+        int ultima = paraula.length()-1;
+
+        boolean a = Character.toLowerCase(paraula.charAt(ultima))=='a';
+        boolean e = Character.toLowerCase(paraula.charAt(ultima))=='e';
+        boolean i = Character.toLowerCase(paraula.charAt(ultima))=='i';
+        boolean o = Character.toLowerCase(paraula.charAt(ultima))=='o';
+        boolean u = Character.toLowerCase(paraula.charAt(ultima))=='u';
         
+        boolean exclamacio = paraula.charAt(ultima)=='!';
+        boolean interrogacio = paraula.charAt(ultima)=='?';
+        boolean punt = paraula.charAt(ultima)=='.';
+
         while (((paraula.isBlank()))==false) {
 
-            int ultima = paraula.length()-1;
+            
 
-            while (paraula.charAt(ultima)=='!'||paraula.charAt(ultima)=='?'||paraula.charAt(ultima)=='.'){
+            while (exclamacio||interrogacio||punt){
                 ultima = ultima - 1;
             }
 
-            if (!((Character.toLowerCase(paraula.charAt(ultima))=='a')||(Character.toLowerCase(paraula.charAt(paraula.length()-1))=='e')||(Character.toLowerCase(paraula.charAt(paraula.length()-1))=='i')||(Character.toLowerCase(paraula.charAt(paraula.length()-1))=='o')||(Character.toLowerCase(paraula.charAt(paraula.length()-1))=='u'))){
+            if (!(a||e||i||o||u)){
                 System.out.println("El lloro diu: "+paraula);
             }   
                 
