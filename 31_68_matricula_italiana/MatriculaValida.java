a1fcea127674140fc0df5a5051827db5f735e3dc
prgtest <cf22javier.garcia@iesjoandaustria.org>
2022-11-10 18:53:33 +0100

    31_68  ∾ prgtest autocommit 31_68 #2

--- 31_68_matricula_italiana/MatriculaValida.java
+++ 31_68_matricula_italiana/MatriculaValida.java
@@ -8,17 +8,30 @@ public class MatriculaValida {
 
         if (paraula.length()==7){
 
+            boolean caracterInvalid = true;
+
+            for (int i = 0; i<=7; i++){
+
+                if (paraula.charAt(i)=='I'||paraula.charAt(i)=='O'||paraula.charAt(i)=='Q'||paraula.charAt(i)=='U'){
+
+                    caracterInvalid = false;
+                    break;
+
+                }
+
+            }
+
             boolean primeresLletres = (paraula.charAt(0)>64 && paraula.charAt(0)<91) && (paraula.charAt(1)>64 && paraula.charAt(1)<91);
             boolean tresNumeros = (paraula.charAt(2)>47 && paraula.charAt(2)<58) && (paraula.charAt(3)>47 && paraula.charAt(3)<58) && (paraula.charAt(4)>47 && paraula.charAt(4)<58);
             boolean ultimesLletres = (paraula.charAt(5)>64 && paraula.charAt(5)<91) && (paraula.charAt(6)>64 && paraula.charAt(6)<91);
 
-            if ((primeresLletres && tresNumeros) && ultimesLletres){
+            if (((primeresLletres && tresNumeros) && ultimesLletres) && caracterInvalid){
 
                 System.out.println("És una matrícula italiana vàlida"); 
 
             }
             else{
-                
+
                 System.out.println("No és una matrícula italiana vàlida"); 
 
             }
