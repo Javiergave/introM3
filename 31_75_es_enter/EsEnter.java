ef5b71adb559c97f42c0e36fa0d9063349316e52
Javier Garcia Vera <cf22javier.garcia@iesjoandaustria.org>
2022-11-11 17:04:56 +0100

    31_75

---
+++ 31_75_es_enter/EsEnter.java
@@ -0,0 +1,43 @@
+import javax.management.monitor.StringMonitorMBean;
+
+/*vagi demanant texts fins rebre la cadena buida.Per 
+cada text no buit que rebi, indicarà si correspon o no a 
+un nombre enter escrit amb dígits. */
+
+public class EsEnter {
+    public static void main(String[] args) {  
+
+        System.out.println("Introdueix texts (enter sol per finalitzar)"); 
+        String paraula = Entrada.readLine();
+        boolean esNombre = true;
+
+        while (paraula.isEmpty()==false){
+
+            paraula = paraula.strip();
+
+            for (int i = 0; i == paraula.length(); i++){
+
+                if (paraula.charAt(i)=='-'||paraula.charAt(i)=='+'){
+                    i = i+1;
+                }
+
+                if (Character.isDigit(paraula.charAt(i))==false){
+
+                esNombre = false;
+
+                }
+
+            }
+
+            if (esNombre){
+                System.out.println("És enter"); 
+            }
+            else{
+                System.out.println("No és enter"); 
+            }
+
+            paraula = Entrada.readLine();
+
+        }
+    }
+}
