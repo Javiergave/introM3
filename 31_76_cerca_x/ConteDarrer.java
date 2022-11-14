24a0b53cca8f8960acf675431d3ffffc63ffa666
Javier Garcia Vera <cf22javier.garcia@iesjoandaustria.org>
2022-11-14 20:12:48 +0100

    31_76

--- 31_76_cerca_x/ConteDarrer.java
+++ 31_76_cerca_x/ConteDarrer.java
@@ -1,3 +1,4 @@
+
 /*vagi demanant texts fins que un no contingui el darrer 
 caràcter del text anterior. El primer text és acceptat 
 sempre a menys que estigui buit.*/
@@ -5,7 +6,38 @@ sempre a menys que estigui buit.*/
 public class ConteDarrer {
     public static void main(String[] args) {  
 
-        
+        System.out.println("Introdueix texts (enter sol per finalitzar)"); 
+        String frase = Entrada.readLine();
+        char ultimaLletra = Character.toLowerCase(frase.charAt(frase.length()-1));
+
+        while (frase.isEmpty()==false){
+
+            System.out.println("bé");
+            frase = Entrada.readLine();
+
+            if (frase.isEmpty()==false){
+
+                if (frase.indexOf(ultimaLletra)>-1){
+
+                    ultimaLletra = Character.toLowerCase(frase.charAt(frase.length()-1));
+
+                }
+                else{
+
+                    break;
+
+                }
+
+            }
+            else{
+
+                break;
+
+            }
+
+        }
+
+        System.out.println("Adéu"); 
 
     }  
 }
