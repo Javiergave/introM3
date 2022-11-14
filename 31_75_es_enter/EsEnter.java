fff04ecddf360e40c68b356e203947d9d2a317e5
prgtest <cf22javier.garcia@iesjoandaustria.org>
2022-11-14 18:48:32 +0100

    31_75  ∾ prgtest autocommit 31_75 #8

--- 31_75_es_enter/EsEnter.java
+++ 31_75_es_enter/EsEnter.java
@@ -20,7 +20,7 @@ public class EsEnter {
              
                 if (paraula.charAt(0)=='-'||paraula.charAt(0)=='+'||Character.isDigit(paraula.charAt(0))){
 
-                    for (int i = 0; (i <= paraula.length()-1)&&(esNombre = true); i++){
+                    for (int i = 01; (i <= paraula.length()-1)&&(esNombre = true); i++){
 
                         esNombre = true;
 
