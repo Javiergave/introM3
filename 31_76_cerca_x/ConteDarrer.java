12170b067b6e8dd5a3f0810208c8e764102db606
Javier Garcia Vera <cf22javier.garcia@iesjoandaustria.org>
2022-11-14 19:41:02 +0100

    new file:   31_76_cerca_x/ConteDarrer.java     modified:   31_75_es_enter/EsEnter.java

---
+++ 31_76_cerca_x/ConteDarrer.java
@@ -0,0 +1,11 @@
+/*vagi demanant texts fins que un no contingui el darrer 
+caràcter del text anterior. El primer text és acceptat 
+sempre a menys que estigui buit.*/
+
+public class ConteDarrer {
+    public static void main(String[] args) {  
+
+        
+
+    }  
+}
