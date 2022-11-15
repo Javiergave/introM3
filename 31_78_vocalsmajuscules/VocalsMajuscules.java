f16bf5548a056f9592a9de104ce029d6e954def9
prgtest <cf22javier.garcia@iesjoandaustria.org>
2022-11-15 16:10:08 +0100

    31_78  ∾ prgtest autocommit 31_78 #2

--- 31_78_vocalsmajuscules/VocalsMajuscules.java
+++ 31_78_vocalsmajuscules/VocalsMajuscules.java
@@ -5,12 +5,13 @@ public class VocalsMajuscules {
         
         System.out.println("Text?"); 
         String paraula = Entrada.readLine();
-        
 
         for (int i= 0; i <= paraula.length()-1; i++){
+
             boolean esVocal = paraula.charAt(i)=='a'||paraula.charAt(i)=='e'||paraula.charAt(i)=='i'||paraula.charAt(i)=='o'||paraula.charAt(i)=='u';
+
             if (Character.isLetter(paraula.charAt(i))&&esVocal==false){
-                System.out.print(paraula.charAt(i)); 
+                System.out.print(Character.toLowerCase(paraula.charAt(i))); 
             }
             else{
                 System.out.print(Character.toUpperCase(paraula.charAt(i))); 
