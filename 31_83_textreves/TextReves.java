427ce8961f6c4041382fec5876b5e90a1ffd2269
Javier Garcia Vera <cf22javier.garcia@iesjoandaustria.org>
2022-11-17 15:10:18 +0100

    31_83

---
+++ 31_83_textreves/TextReves.java
@@ -0,0 +1,19 @@
+/*Aquest programa demana un text i els torna a mostrar però invertint l'ordre dels caràcters 
+que el composen. Els caracters resultants apareixeran separats per comes. */
+public class TextReves {
+    public static void main(String[] args) {  
+
+        System.out.println("Text?");
+        String paraula = Entrada.readLine(); 
+
+        for (int i = paraula.length()-1; i >0; i--){
+
+            if (i==0){
+                System.out.println(paraula.charAt(i)); 
+            }
+            else{
+                System.out.print(" "+paraula.charAt(i)+","); 
+            }
+        }
+    }
+}
