2761b0d320964fd5ae441aa11ddee588f4297ce4
Javier Garcia Vera <cf22javier.garcia@iesjoandaustria.org>
2022-11-17 15:20:10 +0100

    31_84

---
+++ 31_84_textcapicua/Capicua.java
@@ -0,0 +1,31 @@
+/*Aquest programa demana un text i indica si el text és o no capicua. */
+public class Capicua {
+    public static void main(String[] args) {  
+
+        System.out.println("Text?");
+        String paraula = Entrada.readLine(); 
+
+        boolean esCapicua = true;
+
+        for (int i = 0; (i<=paraula.length()-1)&&(esCapicua); i++){
+            for (int j = paraula.length()-1; i>=0; i--){
+
+                if (paraula.charAt(j)!=paraula.charAt(i)){
+
+                    esCapicua=false;
+
+                }
+
+                break;
+
+            }
+        }
+
+        if (esCapicua){
+            System.out.println("És capicua"); 
+        }
+        else{
+            System.out.println("No és capicua"); 
+        }
+    }
+}
