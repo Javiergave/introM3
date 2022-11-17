90de5d8f1b7dcadd91d92bc45a35aa950e23a401
prgtest <cf22javier.garcia@iesjoandaustria.org>
2022-11-17 20:37:28 +0100

    31_87  ∾ prgtest autocommit 31_87 #19

--- 31_87_invertitlletres/TriangleLletresInvertit.java
+++ 31_87_invertitlletres/TriangleLletresInvertit.java
@@ -5,17 +5,22 @@ public class TriangleLletresInvertit {
         System.out.println("Text?"); 
         String paraula = Entrada.readLine(); 
         if (paraula.length()>1){
-            for (int i = paraula.length()-1; i > 0; i--){
+            for (int i = paraula.length(); i>=1; i--){
+            
+                for (int j = 1; j<=i; j++){
+
+                    if (j!=i){
+                        System.out.print(j+", ");
+                    }
+
+                    else {
+                        System.out.print(j); 
+                    }
                 
-                for (int j = i; j < paraula.length()-1; j++){
-                    if (i==j){
-                        System.out.println(paraula.charAt(j)); 
-                    }
-                    else{
-                        System.out.print(paraula.charAt(j)+", "); 
-                    }
                 }
-                
+
+                System.out.println(); 
+
             }
         }
         else{
