4b26a181e625c48e6fa702d65de4fc055854b891
Javier Garcia Vera <cf22javier.garcia@iesjoandaustria.org>
2022-11-17 20:07:37 +0100

    31_87

---
+++ 31_87_invertitlletres/TriangleLletresInvertit.java
@@ -0,0 +1,24 @@
+/*Aquest programa demana un text i dibuixa un triangle amb les lletres del text. */
+public class TriangleLletresInvertit {
+    public static void main(String[] args) {  
+
+        System.out.println("Text?"); 
+        String paraula = Entrada.readLine(); 
+        if (paraula.length()>1){
+            for (int i = paraula.length()-1; i >= 0; i--){
+                for (int j = 0; j <= i;j++){
+                    if (i==j){
+                        System.out.println(paraula.charAt(j)); 
+                    }
+                    else{
+                        System.out.print(paraula.charAt(j)+", "); 
+                    }
+                } 
+            }
+        }
+        else{
+            System.out.println(paraula); 
+        }
+
+    }
+}
