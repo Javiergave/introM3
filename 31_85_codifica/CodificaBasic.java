c83fa2cc49a11db1633ef5e6239962b388ffe0bd
prgtest <cf22javier.garcia@iesjoandaustria.org>
2022-11-17 19:28:10 +0100

    31_85  ∾ prgtest autocommit 31_85 #3

--- 31_85_codifica/CodificaBasic.java
+++ 31_85_codifica/CodificaBasic.java
@@ -14,10 +14,7 @@ public class CodificaBasic {
                 lletra+=1;
                 codificat = codificat + (lletra);
             }
-            if (lletra == 122){
-                lletra = 'a';
-                codificat = codificat + (lletra);
-            }
+            
         }
 
         System.out.println(codificat); 
