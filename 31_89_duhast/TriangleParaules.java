6fb2ad4bab342177feab25a6dd942303d93305f0
prgtest <cf22javier.garcia@iesjoandaustria.org>
2022-11-22 15:42:27 +0100

    intent  ∾ prgtest autocommit 31_89 #6

--- 31_89_duhast/TriangleParaules.java
+++ 31_89_duhast/TriangleParaules.java
@@ -14,7 +14,16 @@ public class TriangleParaules {
                 }
             }
         }
-        int l = 1;
+        int l = 0;
+        for (int i = 0; i<=paraula.length()-1;i++){
+            if (Character.isWhitespace(paraula.charAt(i))){
+                if (Character.isLetter(paraula.charAt(i-1))){
+                    l = i;
+                    break;
+                }
+            }
+        }
+        
         for (int j = 1; j<= cantidadPalabras; j++){
             for (int k = 0; (k<=paraula.length()-1)&&(k<=l); k++){
                 if (Character.isWhitespace(paraula.charAt(k))){
