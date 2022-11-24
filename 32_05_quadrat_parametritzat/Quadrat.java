9a39467595bce9dd4159333e739c097a2e85ca47
prgtest <cf22javier.garcia@iesjoandaustria.org>
2022-11-24 19:21:33 +0100

    32_05  ∾ prgtest autocommit 32_05 #4

--- 32_05_quadrat_parametritzat/Quadrat.java
+++ 32_05_quadrat_parametritzat/Quadrat.java
@@ -3,7 +3,7 @@ public class Quadrat {
     public static void main(String[] args) { 
         int valor = Integer.parseInt(args[0]);  
         if (valor>1){
-            for (int i = 0;i<=valor; i++){
+            for (int i = 0;i<valor; i++){
                 dibuixaLinia();
                 System.out.println(); 
             }
