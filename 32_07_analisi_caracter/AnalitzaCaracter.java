8df44e92dfae130042b6b9b45c831ca2c138bbca
Javier Garcia Vera <cf22javier.garcia@iesjoandaustria.org>
2022-11-24 20:25:25 +0100

    32_07

---
+++ 32_07_analisi_caracter/AnalitzaCaracter.java
@@ -0,0 +1,65 @@
+/*el programa indicarà:
+
+el caràcter que correspon a la posició demanada
+
+la categoria del caràcter:
+
+    lletra
+
+    nombre
+
+    altre: quan no sigui cap dels anteriors
+*/
+
+
+public class AnalitzaCaracter {
+    public static void main(String[] args) {  
+
+        System.out.println("Text?");
+        String paraula = Entrada.readLine(); 
+        System.out.println("Posició?"); 
+        int valor = Integer.parseInt(Entrada.readLine());
+
+        char seleccionat = 'A';
+
+        if (Math.abs(valor)<paraula.length()){
+
+            analisi(seleccionat,paraula,valor);
+            
+        }
+        else{
+
+            System.out.println("Fora de rang"); 
+
+        }
+
+    }
+    public static void analisi(char seleccionat,String paraula,int valor) {  
+        if (valor<0){
+
+            seleccionat = paraula.charAt(paraula.length() - Math.abs(valor));
+
+        }
+        else {
+
+            seleccionat = paraula.charAt(valor);
+
+        }
+
+        if (Character.isLetter(seleccionat)){
+
+            System.out.println("'"+seleccionat+"' és una lletra");
+
+        }
+        else if (Character.isDigit(seleccionat)){
+
+            System.out.println("'"+seleccionat+"' és un nombre"); 
+
+        }
+        else{
+
+            System.out.println("'"+seleccionat+"' és una altra cosa"); 
+
+        }
+    }
+}
