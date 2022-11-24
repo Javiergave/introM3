2daf52e6faa296edc385d07e5fa59621c1511665
Javier Garcia Vera <cf22javier.garcia@iesjoandaustria.org>
2022-11-24 19:16:49 +0100

    32_05

---
+++ 32_05_quadrat_parametritzat/Quadrat.java
@@ -0,0 +1,18 @@
+/*Aquest programa demani un número per entrada estàndard, i dibuixa tants quadrats com indica el número. Si el número és 0 o negatiu, no dibuixarà res. */
+public class Quadrat {
+    public static void main(String[] args) { 
+        int valor = Integer.parseInt(args[0]);  
+        if (valor>0){
+            for (int i = 1;i<=valor; i++){
+                dibuixaLinia();
+            }
+        }
+    }
+    public static void dibuixaLinia(){ 
+        for (int a = 1; a<=5; a++){
+            System.out.print(" X"); 
+        }
+    }
+}
+
+
