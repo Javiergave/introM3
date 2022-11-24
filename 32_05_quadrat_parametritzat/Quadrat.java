4fc51bb991da6b1582b507482665b3e339a43af8
prgtest <cf22javier.garcia@iesjoandaustria.org>
2022-11-24 19:28:38 +0100

    32_05  ∾ prgtest autocommit 32_05 #7

--- 32_05_quadrat_parametritzat/Quadrat.java
+++ 32_05_quadrat_parametritzat/Quadrat.java
@@ -1,18 +1,24 @@
 /*Aquest programa demani un número per entrada estàndard, i dibuixa tants quadrats com indica el número. Si el número és 0 o negatiu, no dibuixarà res. */
 public class Quadrat {
-    public static void main(String[] args) { 
-        int valor = Integer.parseInt(args[0]);  
-        if (valor>=1){
+    public static void main(String[] args) {  
+        int valor = Integer.parseInt(args[0]);
+        if (valor>0){
             for (int i = 1;i<=valor; i++){
-                for (int j = 1;j<=valor; j++){
-                    dibuixaX();
-                }
-                System.out.println(); 
+                dibuixaQuadrat();
             }
         }
     }
-    public static void dibuixaX(){
-        System.out.print(" X"); 
+    
+    public static void dibuixaQuadrat(){
+        for (int a = 1; a<=5; a++){
+            dibuixaLinia();
+            System.out.println(); 
+        }
+    }
+    public static void dibuixaLinia(){ 
+        for (int a = 1; a<=5; a++){
+            System.out.print(" X"); 
+        }
     }
 }
 
