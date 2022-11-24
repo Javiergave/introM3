ad22f36cf0579a3ef643cb438bb69807d434c24f
Javier Garcia Vera <cf22javier.garcia@iesjoandaustria.org>
2022-11-24 18:30:39 +0100

    31_03

---
+++ 32_03_quadrats_modulars/Quadrats.java
@@ -0,0 +1,27 @@
+/*Aquest programa demani un número per entrada estàndard, i dibuixa tants quadrats com indica el número. Si el número és 0 o negatiu, no dibuixarà res. */
+public class Quadrats {
+    public static void main(String[] args) {  
+        System.out.println("Quants?");
+        int valor = Integer.parseInt(Entrada.readLine());
+        if (valor>0){
+            for (int i = 1; i<=valor; i++){
+                dibuixaQuadrats();
+                System.out.println(); 
+
+            }
+        }
+    }
+    public static void dibuixaQuadrats(){ 
+        for (int a = 1; a<=5; a++){
+            dibuixaLinia();
+            System.out.println(); 
+        }
+    }
+    public static void dibuixaLinia(){ 
+        for (int j = 1; j<=5; j++){
+            System.out.print(" X"); 
+        }
+    }
+}
+
+
