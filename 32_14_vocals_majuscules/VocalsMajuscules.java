b56d6cf8b31c8c86cc45f2129ca29e0d9753fb6d
Javier Garcia Vera <cf22javier.garcia@iesjoandaustria.org>
2022-11-29 16:27:07 +0100

    32_14

---
+++ 32_14_vocals_majuscules/VocalsMajuscules.java
@@ -0,0 +1,33 @@
+/*Aquest programa demana un text i mostra totes les lletres en minúscules 
+excepte les vocals, que estaràn en majúscules. */
+public class VocalsMajuscules {
+    public static void main(String[] args) { 
+        
+        System.out.println("Text?"); 
+        String paraula = Entrada.readLine();
+
+        majusculitzaVocals(paraula);
+
+        System.out.println();
+
+    }
+    public static void majusculitzaVocals(String paraula) { 
+        for (int i= 0; i <= paraula.length()-1; i++){
+
+            boolean esVocal = paraula.charAt(i)=='a'||paraula.charAt(i)=='e'||paraula.charAt(i)=='i'||paraula.charAt(i)=='o'||paraula.charAt(i)=='u';
+            boolean esVocalMajus = paraula.charAt(i)=='A'||paraula.charAt(i)=='E'||paraula.charAt(i)=='I'||paraula.charAt(i)=='O'||paraula.charAt(i)=='U';
+
+            if (Character.isLetter(paraula.charAt(i))&&((esVocal==false)&&(esVocalMajus==false))){
+
+                System.out.print(Character.toLowerCase(paraula.charAt(i))); 
+
+            }
+            else{
+
+                System.out.print(Character.toUpperCase(paraula.charAt(i))); 
+                
+            }
+            
+        }
+    }
+}
