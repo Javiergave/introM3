357852901b2e9399936613c06a12c6c789b7edad
Javier Garcia Vera <cf22javier.garcia@iesjoandaustria.org>
2022-11-29 16:29:57 +0100

    32_15

---
+++ 32_15_inicials_majuscules/MajusculitzaInicials.java
@@ -0,0 +1,27 @@
+/*Aquest programademana un text i mostra el mateix text amb les 
+inicials de cada paraula en majúscules i la resta en minúscules. */
+public class MajusculitzaInicials {
+    public static void main(String[] args) {  
+
+        System.out.println("Text?");
+        String paraula = Entrada.readLine(); 
+        
+        majusculitzaInicials(paraula);
+
+    }
+    public static void majusculitzaInicials(String paraula) {  
+        for (int i = 0; i<= paraula.length()-1; i++){
+            
+            if (i==0 || Character.isLetter(paraula.charAt(i-1))==false){
+                System.out.print(Character.toUpperCase(paraula.charAt(i))); 
+            }
+            else{
+
+                System.out.print(Character.toLowerCase(paraula.charAt(i))); 
+
+            }
+
+            
+        }
+    }
+}
