88ec07019a653d20c60197e54cbb502dac1b3d31
prgtest <cf22javier.garcia@iesjoandaustria.org>
2022-12-01 16:52:41 +0100

    32_23  ∾ prgtest autocommit 32_23 #4

--- 32_23_es_vocal/LloroVocalIniciFi.java
+++ 32_23_es_vocal/LloroVocalIniciFi.java
@@ -19,7 +19,7 @@ public class LloroVocalIniciFi {
                     break;
                 }
             }
-            esVocal = ((UtilString.esVocal(paraula.charAt(0)))&&(UtilString.esVocal(paraula.charAt(paraula.length()-1))));
+            esVocal = ((UtilString.esVocal(paraula.charAt(0)))||(UtilString.esVocal(paraula.charAt(paraula.length()-1))));
             if (esVocal){
                 System.out.println("El lloro diu: "+paraula);
             } 
