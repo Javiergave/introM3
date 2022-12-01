519b8cd7e91ab02f4f29b8d483254c15dd383820
Javier Garcia Vera <cf22javier.garcia@iesjoandaustria.org>
2022-12-01 15:37:38 +0100

    32_21

---
+++ 32_21_matricula_italiana/MatriculaValida.java
@@ -0,0 +1,49 @@
+// Aquest programa demana a l'usuari una matrícula i li digui si el que li ha introduït correspon a una matrícula italiana vàlida en el format vigent.
+
+public class MatriculaValida {
+    public static void main(String[] args) {  
+
+        System.out.println("Introduïu una matrícula"); 
+        String paraula = Entrada.readLine(); 
+
+        if (paraula.length()==7){
+
+            char lletra = paraula.charAt(0);
+            boolean lletravalida = true;
+            for (int i = 0; i<=1; i++){
+                lletra = paraula.charAt(i);
+                lletravalida = esLletraValidaPerMatriculaItaliana(lletra);
+            }
+
+            boolean tresNumeros = (paraula.charAt(2)>47 && paraula.charAt(2)<58) && (paraula.charAt(3)>47 && paraula.charAt(3)<58) && (paraula.charAt(4)>47 && paraula.charAt(4)<58);
+
+            for (int i = 5; i<=6; i++){
+                lletra = paraula.charAt(i);
+                lletravalida = esLletraValidaPerMatriculaItaliana(lletra);
+            }
+            boolean ultimesLletres = (paraula.charAt(5)>64 && paraula.charAt(5)<91) && (paraula.charAt(6)>64 && paraula.charAt(6)<91);
+
+            if (lletravalida&&tresNumeros){
+
+                System.out.println("És una matrícula italiana vàlida"); 
+
+            }
+            else{
+
+                System.out.println("No és una matrícula italiana vàlida"); 
+
+            }
+
+        }
+        else{
+
+            System.out.println("No és una matrícula italiana vàlida"); 
+
+        }
+        
+
+    }
+    public static boolean esLletraValidaPerMatriculaItaliana(char lletra) { 
+        return ((lletra>64 && lletra<91)&&(!((lletra == 'I')||(lletra == 'O')||(lletra == 'U')||(lletra == 'Q'))));
+    } 
+}
