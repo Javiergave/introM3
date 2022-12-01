86a82dd198d97177666f8e752249b7216cb6165e
prgtest <cf22javier.garcia@iesjoandaustria.org>
2022-12-01 18:17:22 +0100

    32_23  ∾ prgtest autocommit 32_23 #7

--- 32_23_es_vocal/UtilString.java
+++ 32_23_es_vocal/UtilString.java
@@ -3,7 +3,7 @@ public class UtilString {
 
     /*Comprova si es una vocal */
     public static boolean esVocal(char lletra1) {  
-        String vocals = "aàeèéiíïoóòuúü";
+        String vocals = "aàeèéiíïoóòuúüAÀÁEÉÈUÜÚIÏÍOÓÒ";
         for (int i=0; i < vocals.length(); i++) {
             char lletra2 = vocals.charAt(i);
             if (lletra1 == lletra2) {
