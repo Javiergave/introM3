676700ff6ff88beb69b7f7d8a9befe996ec15ea7
Javier Garcia Vera <cf22javier.garcia@iesjoandaustria.org>
2022-12-01 16:48:43 +0100

    32_23

---
+++ 32_23_es_vocal/LloroVocalIniciFi.java
@@ -0,0 +1,32 @@
+//Aquest programa nomes repeteix les paraules que comencin en vocal minuscula i s'atura quan l'input sigui buit
+public class LloroVocalIniciFi { 
+    public static void main(String[] args) {  
+
+        System.out.println("El lloro demana paraula amb vocal a l'inici o/i final");
+        String paraula = Entrada.readLine();
+        boolean esVocal=false;
+        boolean respostaBool = false;
+        
+        while (((paraula.isBlank()))==false) {
+                
+            if (esVocal){
+                System.out.println("El lloro diu: "+paraula);
+            }   
+                
+            System.out.println("El lloro demana paraula amb vocal a l'inici o/i final");
+            paraula = Entrada.readLine();
+            paraula = paraula.trim();
+            if (paraula.isEmpty()){
+                System.out.println("El lloro demana confirmació per finalitzar");
+                String resposta = Entrada.readLine();  
+                respostaBool = UtilitatsConfirmacio.respostaABoolean(resposta);
+                if (respostaBool){
+                    break;
+                }
+            }
+            esVocal = ((UtilString.esVocal(paraula.charAt(0)))&&(UtilString.esVocal(paraula.charAt(paraula.length()-1))));
+        }
+
+        System.out.println("Adéu");
+    }
+}
---
+++ 32_23_es_vocal/UtilString.java
@@ -0,0 +1,16 @@
+public class UtilString {
+
+
+    /*Comprova si es una vocal */
+    public static boolean esVocal(char lletra1) {  
+        String vocals = "aàeèéiíïoóòuúü";
+        for (int i=0; i < vocals.length(); i++) {
+            char lletra2 = vocals.charAt(i);
+            if (lletra1 == lletra2) {
+                return true;
+            }
+            
+        }
+        return false;
+    }
+}
