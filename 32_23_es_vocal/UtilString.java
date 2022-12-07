d8386598e45b73a0514c2214654acb49dfb626a3
Javier Garcia Vera <cf22javier.garcia@iesjoandaustria.org>
2022-12-07 16:29:10 +0100

    capçaleres

--- 32_23_es_vocal/UtilString.java
+++ 32_23_es_vocal/UtilString.java
@@ -1,7 +1,8 @@
+/*Diverses utilitats per a analitzar i modificar strings */
 public class UtilString {
 
 
-    /*Comprova si es una vocal */
+    /*rep un caràcter i retorna un booleà indicant si el caràcter correspon o no amb una vocal.*/
     public static boolean esVocal(char lletra1) {  
         String vocals = "aàeèéiíïoóòuúüAÀÁEÉÈUÜÚIÏÍOÓÒ";
         for (int i=0; i < vocals.length(); i++) {
