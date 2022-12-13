c7288596ba1ccca97c98223133601e55c2ba1797
prgtest <cf22javier.garcia@iesjoandaustria.org>
2022-12-13 18:53:15 +0100

    32_29  ∾ prgtest autocommit 32_29 #4

--- 32_29_cadena_continua/CadenaContinua.java
+++ 32_29_cadena_continua/CadenaContinua.java
@@ -8,16 +8,15 @@ public class CadenaContinua {
     public static void main(String[] args) {  
 
         System.out.println("Text?"); 
-        String paraula = Entrada.readLine();
+        String paraula1 = Entrada.readLine();
 
-        if (paraula.isEmpty()==false){
+        if (paraula1.isEmpty()==false){
 
             System.out.println("Nombre?"); 
-            
-
+            String paraula = Entrada.readLine(); 
             if(UtilString.esEnter(paraula)){
-                int nombre = Integer.parseInt(Entrada.readLine());
-                System.out.println(UtilString.cadenaContinua(paraula,nombre)); 
+                int nombre = Integer.parseInt(paraula);
+                System.out.println(UtilString.cadenaContinua(paraula1,nombre)); 
             }
 
 
