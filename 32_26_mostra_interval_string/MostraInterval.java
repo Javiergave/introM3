578a19410bb3d6255bfb5e3bb4e6143e104a15ae
Javier Garcia Vera <cf22javier.garcia@iesjoandaustria.org>
2022-12-13 16:06:26 +0100

    32_26

---
+++ 32_26_mostra_interval_string/MostraInterval.java
@@ -0,0 +1,32 @@
+/*Aquest programa demana un text i dos valors enters, i que
+mostra tots els caràcters que hi ha entre el primer i el 
+segon, en l'ordre marcat per l'entrada. */
+
+public class MostraInterval {
+    public static void main(String[] args) { 
+        
+        System.out.println("text?"); 
+        String paraula = Entrada.readLine(); 
+
+        System.out.println("inici?"); 
+        int ini = Integer.parseInt(Entrada.readLine()); 
+
+        System.out.println("final?"); 
+        int fin = Integer.parseInt(Entrada.readLine()); 
+
+        if (fin>paraula.length()-1){
+
+            fin = paraula.length()-1;
+
+        }
+
+        if (ini>paraula.length()-1){
+
+            ini = paraula.length()-1;
+
+        }
+
+        UtilString.intervalString(paraula,ini,fin);
+
+    }
+}
