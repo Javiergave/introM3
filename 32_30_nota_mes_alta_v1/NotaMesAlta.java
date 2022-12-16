70ed9f351125e4b13e01eff13162400aa476407d
prgtest <cf22javier.garcia@iesjoandaustria.org>
2022-12-16 15:39:43 +0100

    32_30  ∾ prgtest autocommit 32_30 #7

--- 32_30_nota_mes_alta_v1/NotaMesAlta.java
+++ 32_30_nota_mes_alta_v1/NotaMesAlta.java
@@ -5,19 +5,18 @@ public class NotaMesAlta {
     public static void main(String[] args) {  
 
         int nota= 0;
-        int notes [] = {0} ;
         int notaMesAlta = 0;
+        int i = 0;
 
         System.out.println("Introdueix les notes (-1 per finalitzar)"); 
-        System.out.println(); 
         
-        for (int i = 1; nota!=-1; i++){
+        for (i = 0; nota!=-1; i++){
             nota = Integer.parseInt(Entrada.readLine()); 
             if (nota == -1){
                 break;
             }
-            else{
-                notes[i] = nota;
+            else if (nota>notaMesAlta){
+                notaMesAlta = nota;
             }
 
 
@@ -27,18 +26,11 @@ public class NotaMesAlta {
 
         
 
-        if (notes.length <2){
+        if (i <2){
             System.out.println("Com a minim calen dues notes"); 
         }
 
         else{
-            for (int i=0; i<notes.length; i++){
-
-                if (notes[i]>notaMesAlta){
-                    notaMesAlta = notes[i];
-                }
-                
-            }
 
             System.out.println("La nota més alta és "+notaMesAlta);
 
