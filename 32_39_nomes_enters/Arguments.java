82ccc4b13c63676108c66b9a16711f979b700290
Javier Garcia Vera <cf22javier.garcia@iesjoandaustria.org>
2023-01-10 16:37:23 +0100

    32_39

---
+++ 32_39_nomes_enters/Arguments.java
@@ -0,0 +1,28 @@
+/*Aquest programa mostra el resultat de sumar els enters que rep per comandes.*/
+
+public class Arguments {
+
+    public static void main(String[] args) {
+
+        if(args.length==0){
+            System.out.println("Cap argument"); 
+        }
+        else{
+
+            int resultat=0;
+
+            int quantitatEnters = UtilString.quantsEnters(args);
+
+            int[] numeros;
+
+            numeros = new int[quantitatEnters];
+
+            numeros = UtilString.filtraEnters(args);
+
+            resultat = UtilString.sumaEnters(numeros);
+
+            System.out.println(resultat); 
+        }
+        
+    }
+}
