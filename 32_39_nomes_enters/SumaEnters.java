2c00a16fbf274e531922f81408b5b0771664a897
prgtest <cf22javier.garcia@iesjoandaustria.org>
2023-01-11 16:33:54 +0100

    32_39  ∾ prgtest autocommit 32_39 #6

--- 32_39_nomes_enters/SumaEnters.java
+++ 32_39_nomes_enters/SumaEnters.java
@@ -8,13 +8,53 @@ public class SumaEnters {
 
         int[] numeros;
 
-        numeros = new int[UtilString.quantsEnters(args)];
+        numeros = new int[quantsEnters(args)];
 
-        numeros = UtilString.filtraEnters(args);
+        numeros = filtraEnters(args);
 
-        resultat = UtilString.sumaEnters(numeros);
+        resultat = sumaEnters(numeros);
 
         System.out.println(resultat); 
     }
-        
+    /*Aquest programa crea un valor amb la quantitar d'enters que hi ha */
+    public static int quantsEnters(String[] valors) {
+        int quantsEnters = 0;
+    
+        for (int i = 0;i<valors.length;i++){
+            if (UtilString.esEnter(valors[i])){
+                quantsEnters++;
+            }
+        }
+        return quantsEnters;
+    } 
+    
+    /*Aquest programa filtra els enters que hi ha a l'array rebut */
+    public static int[] filtraEnters(String[] valors) { 
+        int quantsEnters = quantsEnters(valors);
+
+        int[] numeros;
+    
+        numeros = new int[quantsEnters];
+    
+        for (int i=0; i<numeros.length;i++){
+            for (int j = 0; j<valors.length;j++){
+                if (UtilString.esEnter(valors[j])){
+                    numeros[i] = Integer.parseInt(valors[j]);
+                    valors[j] = "a";
+                    break;
+                }
+                
+            }
+        }
+        return numeros;
+    }
+    
+    /*Aquest programa suma un array d'enters */
+    public static int sumaEnters(int[] valors) {
+        int resultat = 0;
+        for (int i = 0; i<valors.length;i++){
+            resultat = resultat + valors[i];
+        }
+        return resultat;
+    }
 }
--- 32_39_nomes_enters/UtilString.java
+++ 32_39_nomes_enters/UtilString.java
@@ -3,48 +3,6 @@
 
 public class UtilString {
 
-    /*Aquest programa crea un valor amb la quantitar d'enters que hi ha */
-    public static int quantsEnters(String[] valors) {
-        int quantsEnters = 0;
-
-        for (int i = 0;i<valors.length;i++){
-            if (esEnter(valors[i])){
-                quantsEnters++;
-            }
-        }
-        return quantsEnters;
-    } 
-
-    /*Aquest programa filtra els enters que hi ha a l'array rebut */
-    public static int[] filtraEnters(String[] valors) { 
-        int quantsEnters = quantsEnters(valors);
-
-        int[] numeros;
-
-        numeros = new int[quantsEnters];
-
-        for (int i=0; i<numeros.length;i++){
-            for (int j = 0; j<valors.length;j++){
-                if (esEnter(valors[j])){
-                    numeros[i] = Integer.parseInt(valors[j]);
-                    valors[j] = "a";
-                    break;
-                }
-                
-            }
-        }
-        return numeros;
-    }
-
-    /*Aquest programa suma un array d'enters */
-    public static int sumaEnters(int[] valors) {
-        int resultat = 0;
-        for (int i = 0; i<valors.length;i++){
-            resultat = resultat + valors[i];
-        }
-        return resultat;
-    }
-
     /*Aquest programa retorna els elements de l'array entrat separats amb el char escollit */
     public static String entreComes(int numeros[], char separador){
         String textFinal = "";
