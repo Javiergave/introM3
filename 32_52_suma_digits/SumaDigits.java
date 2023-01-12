0ee948cf0afa73ee21d16fa4ff3c4fbb6edecf2c
Javier Garcia Vera <cf22javier.garcia@iesjoandaustria.org>
2023-01-12 20:26:35 +0100

    32_52

---
+++ 32_52_suma_digits/SumaDigits.java
@@ -0,0 +1,34 @@
+/* Aquest programa demana un text i mostra la suma dels digits que conté */
+public class SumaDigits {
+    public static void main(String[] args){
+        System.out.println("Text?");
+        String text = Entrada.readLine();
+        int nombres = sumaDigits(text);
+        System.out.println(nombres);
+    }
+
+       // extrae los numeros y los suma entre ellos
+    public static int sumaDigits(String text) {
+        // cas base
+        if (text.isEmpty()){
+            return 0;
+        }
+
+        // tracta pas actual
+        char numero = text.charAt(0);
+        int total = 0;
+        int nombre = Integer.parseInt(String.valueOf(numero));
+        if (Character.isDigit(text.charAt(0))){
+            total = total + nombre;
+        }
+
+        // tracta pas recursiu
+        String resta = text.substring(1);
+        total = total + sumaDigits(resta);
+
+        // composa resultat
+        return total;
+    }
+}
+   
+   
