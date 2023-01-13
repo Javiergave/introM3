4e70d83f265a47a1ce14bb34ae0c68025075afa6
Javier Garcia Vera <cf22javier.garcia@iesjoandaustria.org>
2023-01-13 16:01:10 +0100

    32_53

---
+++ 32_53_preordre/Subcadenes.java
@@ -0,0 +1,47 @@
+import java.util.concurrent.Flow.Subscriber;
+
+import javax.swing.event.MouseInputListener;
+
+/*Aquest programa mostra una string que rebi de manera recursiva de la seguent manera: 
+Primer es mostra la cadena
+Després es mostra les subcadenes corresponents a la primera meitat de la cadena
+Finalment es mostra les subcadenes corresponents a la resta de la cadena
+*/
+public class Subcadenes {
+    public static void main(String[] args){
+        System.out.println("Text?");
+        String text = Entrada.readLine();
+        String senseEspais = "";
+        char lletra = 'a';
+        for (int i = 0; i<=text.length()-1;i++){
+            lletra = text.charAt(i);
+            if ((lletra==' ')){
+                continue; 
+            }else{
+                senseEspais = senseEspais + text.charAt(i);
+            }
+            
+        }
+        mostraCadena(senseEspais);
+    }
+    
+       // extrae los numeros y los suma entre ellos
+    public static void mostraCadena(String text) {
+        // cas base
+        if (text.isEmpty()||text.length()==1){
+            System.out.println(text); 
+            return;
+        }
+    
+        // tracta pas actual
+        System.out.println(text); 
+        String meitat =  text.substring(0,(text.length()/2));
+        String meitatfinal = text.substring((text.length()/2),text.length()-1);
+    
+        // tracta pas recursiu
+        mostraCadena(meitat);     
+        mostraCadena(meitatfinal); 
+        // composa resultat
+        return;
+    }
+}
