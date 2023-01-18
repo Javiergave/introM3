40f6c739f2678448d902f2ea22be27f9eeb00cc1
prgtest <cf22javier.garcia@iesjoandaustria.org>
2023-01-18 18:58:19 +0100

    32_45  ∾ prgtest autocommit 32_45 #1

--- 32_45_digitart/UtilTaula.java
+++ 32_45_digitart/UtilTaula.java
@@ -3,8 +3,8 @@ public class UtilTaula {
     public static char[][] substitueix(char[][] origen, char x,char  ch){
         char[][] substitueix = new char[6][7];
         substitueix = origen;
-        for (int i = 0; i<=5; i++){
-            for (int j = 0; j<=6; j++){
+        for (int i = 0; i<=4; i++){
+            for (int j = 0; j<=5; j++){
 
                 if (origen[i][j]==x){
                     substitueix[i][j] = ch;
