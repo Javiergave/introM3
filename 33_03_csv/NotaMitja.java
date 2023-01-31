7008072aabfa522f76bbef663bed205cd8421cee
Javier Garcia Vera <cf22javier.garcia@iesjoandaustria.org>
2023-01-31 18:57:08 +0100

    33_3

--- 33_03_csv/NotaMitja.java
+++ 33_03_csv/NotaMitja.java
@@ -2,7 +2,7 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-public class ProcesaLiniaCSV {
+public class NotaMitja {
     public static void main(String[] args) throws IOException  {
 
         String cami = "notes.csv";
