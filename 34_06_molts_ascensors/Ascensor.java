744120a3fbaefef6e204e8d833323e35f61b9230
Javier Garcia Vera <cf22javier.garcia@iesjoandaustria.org>
2023-02-09 20:35:33 +0100

    34_06

---
+++ 34_06_molts_ascensors/Ascensor.java
@@ -0,0 +1,33 @@
+//Aquest programa mostra el pisos on són el ascensors, hi ha tants ascensors com vulgui l'usuari
+public class Ascensor {
+    static int pis;
+    public static Ascensor[] creaAscensors(int quants) {
+                
+        Ascensor[] ascensor = new Ascensor[quants];
+        for (int i = 0; i<=quants;i++){
+            ascensor = new Ascensor[i];
+            ascensor[i].pis=i;
+        }
+        return ascensor;
+    }
+    public static void main(String[] args){
+        int quants = 0;
+             
+        if(UtilString.esEnter(args[0])){
+            quants = Integer.parseInt(args[0]);
+        }
+                
+        if(quants == 0){
+            System.out.println("Cap ascensor"); 
+        }
+        else{
+            Ascensor[] ascensors = creaAscensors(quants);
+            
+            for (int i = 0; i < ascensors.length; i++) {
+
+                System.out.printf("Ascensor %d al pis %d%n", i, ascensors[i].pis);
+            
+            }
+        }
+    }
+}
