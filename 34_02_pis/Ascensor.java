2f9eba97a36a5e69c96dc13573219b85565fe6a8
Javier Garcia Vera <cf22javier.garcia@iesjoandaustria.org>
2023-02-09 15:03:00 +0100

    34:02

---
+++ 34_02_pis/Ascensor.java
@@ -0,0 +1,9 @@
+//Aquest programa és una representació d'un ascensor
+public class Ascensor{ 
+    static int pis = -1;
+    public static void main(String[] args) {  
+        Ascensor ascensor;
+        ascensor = new Ascensor();
+        System.out.println("L'ascensor està a la planta "+ascensor.pis); 
+    }
+}
