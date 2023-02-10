b478805b30e19ecaa0bb7eb32c2ecd8a52a7a724
Javier Garcia Vera <cf22javier.garcia@iesjoandaustria.org>
2023-02-10 16:48:01 +0100

    34_13

---
+++ 34_13_ascensor_protegit/Ascensor.java
@@ -0,0 +1,21 @@
+//Aquest programa és una representació d'un ascensor
+public class Ascensor{ 
+    private int pis = -1;
+    private String moviment = "aturat";
+    public int getPis() {  
+        return pis;
+    }
+    public String getMoviment() {  
+        return moviment;
+    }
+    public void setPis(int nouPis) {  
+        if(nouPis>-1&&nouPis<10){
+            pis = nouPis;
+        }
+    }
+    public void setMoviment(String nouMoviment) {  
+        if(nouMoviment.equals("pujant")||nouMoviment.equals("baixant")||nouMoviment.equals("aturat")){
+            moviment = nouMoviment; 
+        }
+    }
+}
