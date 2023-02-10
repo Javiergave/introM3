5ef5d790152803fa9b343b127d5b887cad7f4d51
Javier Garcia Vera <cf22javier.garcia@iesjoandaustria.org>
2023-02-10 17:10:21 +0100

    34_14

---
+++ 34_14_com_esta/GatRenat.java
@@ -0,0 +1,28 @@
+//Aquest programa és una representació d'un ascensor
+public class GatRenat{ 
+    private int vides = 7;
+    private String posicio = "estirat";
+
+    public boolean estaDret() {  
+        return posicio.equals("dret");
+    }
+    public boolean estaAssegut() {
+        return posicio.equals("assegut");
+    }
+    public boolean estaEstirat() {
+        return posicio.equals("estirat");
+    }
+    public boolean estaViu() {
+        return vides>0;
+    }
+    public void setVides(int novesVides){
+        if(novesVides>=0){
+            vides=novesVides;
+        }
+    }
+    public void setPosicio(String novaPosicio) {
+        if(novaPosicio.equals("dret")||novaPosicio.equals("assegut")||novaPosicio.equals("estirat")){
+            posicio=novaPosicio;
+        }
+    }
+}
