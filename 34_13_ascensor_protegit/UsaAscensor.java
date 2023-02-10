eddf7fd32589c22a9c43c2332b7cc0c795500b07
Javier Garcia Vera <cf22javier.garcia@iesjoandaustria.org>
2023-02-10 16:48:53 +0100

    34_13

--- 34_13_ascensor_protegit/UsaAscensor.java
+++ 34_13_ascensor_protegit/UsaAscensor.java
@@ -3,9 +3,9 @@
 public class UsaAscensor {
     public static void main(String[] args) {  
         Ascensor ascensor = new Ascensor();
-        System.out.println("Pis inicial: " + ascensor.getPis);
+        System.out.println("Pis inicial: " + ascensor.getPis());
 
-         System.out.println("Moviment inicial: " + ascensor.getMoviment);
+         System.out.println("Moviment inicial: " + ascensor.getMoviment());
 
          System.out.println("Introdueix nou pis:");
 
@@ -15,9 +15,9 @@ public class UsaAscensor {
 
          ascensor.setMoviment(Entrada.readLine());
 
-         System.out.println("Pis final: " + ascensor.getPis);
+         System.out.println("Pis final: " + ascensor.getPis());
 
-         System.out.println("Moviment final: " + ascensor.getMoviment);
+         System.out.println("Moviment final: " + ascensor.getMoviment());
     }
     
 }
