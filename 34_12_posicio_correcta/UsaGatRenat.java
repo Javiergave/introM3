645a428cf9ccebeb7d160d2ec852cb6e5013b52b
Javier Garcia Vera <cf22javier.garcia@iesjoandaustria.org>
2023-02-10 16:39:27 +0100

    34_11

---
+++ 34_12_posicio_correcta/UsaGatRenat.java
@@ -0,0 +1,15 @@
+/*Aquest programa permet utilitzar l'ascensor */
+
+public class UsaGatRenat {
+    public static void main(String[] args) {  
+        GatRenat renat = new GatRenat();
+        System.out.println("Vides inicials: " + renat.getVides());
+
+        System.out.println("Posició inicial: " + renat.getPosicio());
+
+        renat.setPosicio("assegut");
+
+        System.out.println("Posició final: " + renat.getPosicio()); 
+    }
+    
+}
