34a1b57f3b154e1e23ee6cd43133a05502b68d18
prgtest <cf22javier.garcia@iesjoandaustria.org>
2023-02-10 15:53:32 +0100

    34_08  ∾ prgtest autocommit 34_08 #1

--- 34_08_desdefora/UsaAscensor.java
+++ 34_08_desdefora/UsaAscensor.java
@@ -3,7 +3,7 @@
 public class UsaAscensor {
     public static void main(String[] args) {  
         Ascensor ascensor = new Ascensor();
-        System.out.println("L'ascensor desde fora està al pis "+ascensor.getPis()); 
+        System.out.println("L'ascensor creat des de fora està al pis "+ascensor.getPis()); 
     }
     
 }
