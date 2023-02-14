8a2ae9788c25db2316a743544ed4f4be3ac784d6
Javier Garcia Vera <cf22javier.garcia@iesjoandaustria.org>
2023-02-14 18:13:35 +0100

    34_18

---
+++ 34_18_ascensor_interactiu/Ascensor.java
@@ -0,0 +1,105 @@
+import javax.sql.StatementEventListener;
+
+//Aquest programa és una representació d'un ascensor
+public class Ascensor{ 
+    private int pis = -1;
+    private String moviment = "aturat";
+
+    public boolean aturat(){
+        if(moviment.equals("aturat")){
+            return true;
+        }
+        setMoviment("aturat");
+        return true;
+    }
+    public boolean arrencaAmunt(){
+        if(pis==10){
+            return true;
+        }
+        setPis(10);
+        return true;
+    }
+    public boolean arrencaAbaix(){
+        if(pis==-1){
+            return true;
+        }
+        setPis(-1);
+        return true;
+    }
+    public int seguentPis(){
+        if(moviment.equals("pujant")){
+            if(pis==10){
+                setPis(9);
+                setMoviment("baixant");
+            }
+            setPis(pis+1);
+        }
+        if(moviment.equals("baixant")){
+            if(pis==10&&moviment.equals("baiixant")){
+                setPis(0);
+                setMoviment("pujant");
+            }
+            setPis(pis-1);
+        }
+        return pis;
+    }
+    public boolean estaAbaix (){
+        if (pis==-1){
+            return true;
+        }
+        return false;
+    }
+    public boolean estaAmunt (){
+        if (pis==10){
+            return true;
+        }
+        return false;
+    }
+
+    public boolean estaAturat (){
+        if (moviment.equals("aturat")){
+            return true;
+        }
+        return false;
+    }
+    public boolean estaMovent (){
+        if(!moviment.equals("aturat")){
+            return true;
+        }
+        return false;
+    }
+
+    public boolean estaPujant (){
+        if (moviment.equals("pujant")){
+            return true;
+        }
+        return false;
+    }
+    public boolean estaBaixant (){
+        if (moviment.equals("baixant")){
+            return true;
+        }
+        return false;
+    }
+
+    public String comEsta(){
+        return (moviment+" al pis "+pis);
+    }
+
+    public String getMoviment(){
+        return moviment;
+    }
+    public int getPis(){
+        return pis;   
+    }
+    public void setPis(int nouPis){
+        if(nouPis>=-1&&nouPis<=10){
+            pis=nouPis;
+        }
+    }
+    public void setMoviment(String nouMoviment) {
+        if(nouMoviment.equals("pujant")||nouMoviment.equals("baixant")||nouMoviment.equals("aturat")){
+            moviment=nouMoviment;
+        }
+    }
+}
