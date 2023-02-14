8d3aa2cd3a40b5cf37ec201b85e11471ad76a9ca
Javier Garcia Vera <cf22javier.garcia@iesjoandaustria.org>
2023-02-14 16:29:41 +0100

    34_15

--- 34_15_ascensor_informatiu/Ascensor.java
+++ 34_15_ascensor_informatiu/Ascensor.java
@@ -3,6 +3,49 @@ public class Ascensor{
     private int pis = -1;
     private String moviment = "aturat";
 
+    public boolean estaAbaix (int pis){
+        if (pis==-1){
+            return true;
+        }
+        return false;
+    }
+    public boolean estaAmunt (int pis){
+        if (pis==10){
+            return true;
+        }
+        return false;
+    }
+
+    public boolean estaAturat (String moviment){
+        if (moviment.equals("aturat")){
+            return true;
+        }
+        return false;
+    }
+    public boolean estaMovent (String moviment){
+        if(!moviment.equals("aturat")){
+            return true;
+        }
+        return false;
+    }
+
+    public boolean estaPujant (String moviment){
+        if (moviment.equals("pujant")){
+            return true;
+        }
+        return false;
+    }
+    public boolean estaBaixant (String moviment){
+        if (moviment.equals("baixant")){
+            return true;
+        }
+        return false;
+    }
+
+    public String comEsta(String moviment,int pis){
+        return (moviment+" al pis "+pis);
+    }
+
     public String getMoviment(){
         return moviment;
     }
