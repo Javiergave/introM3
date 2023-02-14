00b401cbba227a9c498a25a2b45dbcc8072c2065
Javier Garcia Vera <cf22javier.garcia@iesjoandaustria.org>
2023-02-14 16:41:35 +0100

    34_16

---
+++ 34_16_ensinistrat/UsaGatRenat.java
@@ -0,0 +1,17 @@
+/*Aquest programa permet utilitzar l'ascensor */
+
+public class UsaGatRenat {
+    public static void main(String[] args) {  
+        GatRenat renat = new GatRenat();
+
+        System.out.println("El Renat diu: "+ renat.aixecat());
+
+        System.out.println("El Renat diu: "+ renat.seu());
+
+        System.out.println("El Renat diu: "+ renat.estirat());
+
+        System.out.println("El Renat diu: "+ renat.estirat());
+
+    }
+    
+}
