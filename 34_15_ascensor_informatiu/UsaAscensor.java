c3d776cc7fbc1f84f62834909e20d52cd3d5e550
prgtest <cf22javier.garcia@iesjoandaustria.org>
2023-02-14 16:16:38 +0100

    34_15  ∾ prgtest autocommit 34_15 #2

--- 34_15_ascensor_informatiu/UsaAscensor.java
+++ 34_15_ascensor_informatiu/UsaAscensor.java
@@ -4,7 +4,7 @@ public class UsaAscensor {
     public static void main(String[] args) {  
         Ascensor ascensor = new Ascensor();
 
-        System.out.println("Pis incial: " + ascensor.getPis());
+        System.out.println("Pis inicial: " + ascensor.getPis());
 
         System.out.println("Moviment inicial: " + ascensor.getMoviment());
 
