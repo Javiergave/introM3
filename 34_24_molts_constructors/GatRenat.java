7003246cccc8940f4feb1376ebcb7853aeb0526d
prgtest <cf22javier.garcia@iesjoandaustria.org>
2023-02-16 19:03:26 +0100

    34_24  ∾ prgtest autocommit 34_24 #1

--- 34_24_molts_constructors/GatRenat.java
+++ 34_24_molts_constructors/GatRenat.java
@@ -1,14 +1,31 @@
 //Aquest és el gat Renat, te vides i és a una posicio
 public class GatRenat {
 
-    private int vides = 7;
+    private int vides = 8;
     private String posicio = "estirat";
-    public GatRenat(int vides,String posicio) {
+    public GatRenat(int novesVides,String novaPosicio) {
+
+        setVides(novesVides);
+        setPosicio(novaPosicio);
+
+    }
+    public GatRenat(int novesVides) {
+
+        setVides(novesVides);
+
+    }
+    public GatRenat(String novaPosicio) {
+
+        setPosicio(novaPosicio);
+
+    }
+    public GatRenat() {
 
         setVides(vides);
         setPosicio(posicio);
 
     }
+    
 
     public int getVides() { return vides; }
 
@@ -39,7 +56,24 @@ public class GatRenat {
 
     public static void main(String[] args) {
 
-        System.out.println(new GatRenat(7, "dret"));
+        GatRenat[] renats = {
+    
+            new GatRenat(),         // tot per defecte
+    
+            new GatRenat(8),        // 8 vides i posició per defecte
+    
+            new GatRenat("dret"),   // posició dret i vides per defecte
+    
+            new GatRenat(8, "dret") // 8 vides i posició dret
+    
+    
+        };
+    
+        for (GatRenat renat: renats) {
+    
+            System.out.println(renat);
+    
+        }
+    
     }
-
 }
