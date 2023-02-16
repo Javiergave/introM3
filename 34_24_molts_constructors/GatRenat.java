1cbc8e51edce88bd65fd27b6755264c2b5c8dd87
Javier Garcia Vera <cf22javier.garcia@iesjoandaustria.org>
2023-02-16 19:01:35 +0100

    34_24

---
+++ 34_24_molts_constructors/GatRenat.java
@@ -0,0 +1,45 @@
+//Aquest és el gat Renat, te vides i és a una posicio
+public class GatRenat {
+
+    private int vides = 7;
+    private String posicio = "estirat";
+    public GatRenat(int vides,String posicio) {
+
+        setVides(vides);
+        setPosicio(posicio);
+
+    }
+
+    public int getVides() { return vides; }
+
+    public void setVides(int novesVides) { 
+
+        if (novesVides >= 0)  {
+
+            vides = novesVides;
+
+        }
+
+    }
+
+    public String getPosicio() { return posicio; }
+
+    public void setPosicio(String novaPosicio) { 
+
+        if (novaPosicio.equals("dret")||novaPosicio.equals("assegut")||novaPosicio.equals("estirat"))  {
+
+            posicio = novaPosicio;
+
+        }
+
+    }
+    @Override
+
+    public String toString() { return String.format("Vides: %d. Posició: %s", vides,posicio); }
+
+    public static void main(String[] args) {
+
+        System.out.println(new GatRenat(7, "dret"));
+    }
+
+}
