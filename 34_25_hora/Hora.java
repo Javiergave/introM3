25f29c52d73aca2d8dc60b563d51b395e171481f
prgtest <cf22javier.garcia@iesjoandaustria.org>
2023-02-17 17:17:32 +0100

    Merge branch 'main' of github.com:Javiergave/introM3  ∾ prgtest autocommit 34_25 #22

--- 34_25_hora/Hora.java
+++ 34_25_hora/Hora.java
@@ -115,7 +115,7 @@ public class Hora {
                 setSegons(59);
             }
             else{
-                setMinuts(getMinuts()-(decrementa/60));
+                setMinuts(getMinuts()-(1-decrementa/60));
             }
             setSegons(59-((decrementa*(decrementa/60))));
         }else{
