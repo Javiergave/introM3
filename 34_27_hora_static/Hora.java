6b8596b5fd6b805b572c24095565f65ee991fc96
prgtest <cf22javier.garcia@iesjoandaustria.org>
2023-02-22 18:43:08 +0100

    34_27  ∾ prgtest autocommit 34_27 #2

--- 34_27_hora_static/Hora.java
+++ 34_27_hora_static/Hora.java
@@ -208,9 +208,9 @@ public class Hora {
         return 0;
     }
     public int compareTo(Hora hora1){
-        int totalHoraOtro = this.hores*3600 + this.minuts*60 + this.segons;
         int totalHoraEste = hora1.hores*3600 + hora1.minuts*60 + hora1.segons;
-        
+        int totalHoraOtro = this.hores*3600 + this.minuts*60 + this.segons;
+
         if(totalHoraEste<totalHoraOtro){
             return -1;
         }
