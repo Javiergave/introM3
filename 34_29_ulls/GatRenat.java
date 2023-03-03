236579983a46e15a3cd2eb5af699318d98b0a6a9
prgtest <cf22javier.garcia@iesjoandaustria.org>
2023-03-03 23:58:07 +0100

    34_29  ∾ prgtest autocommit 34_29 #4

--- 34_29_ulls/GatRenat.java
+++ 34_29_ulls/GatRenat.java
@@ -112,7 +112,7 @@ public class GatRenat {
         setVides(novesVides);
 
     }
-    private GatRenat(String novaPosicio) {
+    public GatRenat(String novaPosicio) {
 
         setPosicio(novaPosicio);
 
