2636f00f34373cd86e1ffcf5cb4850eda585d61e
prgtest <cf22javier.garcia@iesjoandaustria.org>
2023-03-07 18:30:15 +0100

    34_30  ∾ prgtest autocommit 34_30 #9

--- 34_30_picarol/GatRenat.java
+++ 34_30_picarol/GatRenat.java
@@ -14,7 +14,7 @@ public class GatRenat {
     }
 
     public Picarol posaPicarol(Picarol picarol){
-        if(picarol==null){
+        if(this.picarol==null){
             return null;
         }
         return this.picarol = new Picarol();
