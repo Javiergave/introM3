0d7e9597e14e2a96abfe7d6df54fa372cceeeca4
prgtest <cf22javier.garcia@iesjoandaustria.org>
2023-03-09 17:01:49 +0100

    Actualització automàtica de l'informe de resultats ∾ prgtest autocommit 34_30 #5

--- 34_30_picarol/Picarol.java
+++ 34_30_picarol/Picarol.java
@@ -16,7 +16,7 @@ public class Picarol{
     public void sona(){
         cops+=1;
 
-        for(int i = 0; i<cops;i++){
+        for(int i = 0; i<=cops;i++){
             System.out.println("clink-clink"); 
             
         }
