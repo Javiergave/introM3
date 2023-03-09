322411fe632c3b2e556f2dceb22835dcc88ab2a5
Javier Garcia Vera <cf22javier.garcia@iesjoandaustria.org>
2023-03-09 17:06:59 +0100

    commentaris

--- 34_29_ulls/UllDeGat.java
+++ 34_29_ulls/UllDeGat.java
@@ -1,3 +1,10 @@
+/*
+ * Copyright 2022 Javier García Vera
+ * 
+ * This is free software, licensed under the GNU General Public License V3.
+ * See http://www.gnu.org/licenses/gpl.html for more information
+ * 
+ */
 /*Funcions i característiques dels ulls d'un gat */
 public class UllDeGat{
     public UllDeGat ull;
