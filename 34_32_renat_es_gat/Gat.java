cb5a525c8c62870a11fc455cfc1ded5f781e6438
Javier Garcia Vera <cf22javier.garcia@iesjoandaustria.org>
2023-03-10 16:51:58 +0100

    34_32

---
+++ 34_32_renat_es_gat/Gat.java
@@ -0,0 +1,30 @@
+/*
+ * Copyright 2022 Javier García Vera
+ * 
+ * This is free software, licensed under the GNU General Public License V3.
+ * See http://www.gnu.org/licenses/gpl.html for more information
+ * 
+ */
+
+//Aquest és un gat, te vides i és a una posicio
+class Gat{
+    private int vides = 7;
+    private String posicio = "estirat";
+
+    public String getPosicio() {  
+        return posicio;
+    }
+    public int getVides() {
+        return vides;
+    }
+    public void setVides(int novesVides){
+        if(novesVides>=0){
+            vides=novesVides;
+        }
+    }
+    public void setPosicio(String novaPosicio) {
+        if(novaPosicio.equals("dret")||novaPosicio.equals("assegut")||novaPosicio.equals("estirat")){
+            posicio=novaPosicio;
+        }
+    }
+}
--- 34_32_renat_es_gat/GatRenat.java
+++ 34_32_renat_es_gat/GatRenat.java
@@ -5,28 +5,7 @@
  * See http://www.gnu.org/licenses/gpl.html for more information
  * 
  */
-//Aquest és el gat Renat, te vides i és a una posicio
-class Gat{
-    private int vides = 7;
-    private String posicio = "estirat";
 
-    public String getPosicio() {  
-        return posicio;
-    }
-    public int getVides() {
-        return vides;
-    }
-    public void setVides(int novesVides){
-        if(novesVides>=0){
-            vides=novesVides;
-        }
-    }
-    public void setPosicio(String novaPosicio) {
-        if(novaPosicio.equals("dret")||novaPosicio.equals("assegut")||novaPosicio.equals("estirat")){
-            posicio=novaPosicio;
-        }
-    }
-}
 //Aquest es el Gat Renat, te més funcions que un gat normal
 public class GatRenat extends Gat{ 
 
