cb7e455df1b52cb79c3ba8c6f9428cb5e44e8512
Javier Garcia Vera <cf22javier.garcia@iesjoandaustria.org>
2023-03-14 16:30:52 +0100

    34_34

---
+++ 34_34_garfield/Gat.java
@@ -0,0 +1,70 @@
+/*
+ * Copyright 2022 Javier García Vera
+ * 
+ * This is free software, licensed under the GNU General Public License V3.
+ * See http://www.gnu.org/licenses/gpl.html for more information
+ * 
+ */
+
+//Aquest és un gat, te vides i és a una posicio
+class Gat{
+    private int vides = 7;
+    private String posicio = "estirat";
+
+    public String getPosicio() {  
+        return posicio;
+    }
+    public int getVides() {
+        return vides;
+    }
+    public void setVides(int novesVides){
+        if(novesVides>=0){
+            vides=novesVides;
+        }
+    }
+    public void setPosicio(String novaPosicio) {
+        if(novaPosicio.equals("dret")||novaPosicio.equals("assegut")||novaPosicio.equals("estirat")){
+            posicio=novaPosicio;
+        }
+    }
+    public String aixecat(){
+        if(!estaDret()){
+            this.setPosicio("dret");
+            return ("m'aixeco"); 
+        }
+        else{
+            return ("no faig res"); 
+        }
+    }
+    public String seu(){
+        if(!estaAssegut()){
+            this.setPosicio("assegut");
+            return ("m'assec"); 
+        }
+        else{
+            return ("no faig res"); 
+        }
+    }
+    public String estirat(){
+        if(!estaEstirat()){
+            this.setPosicio("estirat");
+            return ("m'estiro"); 
+        }
+        else{
+            return ("no faig res"); 
+        }
+    }
+
+    public boolean estaDret() {  
+        return getPosicio().equals("dret");
+    }
+    public boolean estaAssegut() {
+        return getPosicio().equals("assegut");
+    }
+    public boolean estaEstirat() {
+        return getPosicio().equals("estirat");
+    }
+    public boolean estaViu() {
+        return (getVides()>0);
+    }
+}
