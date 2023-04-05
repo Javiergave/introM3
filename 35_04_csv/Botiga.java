b277582adfb6da9afa791b5faa6c26704a6b46f9
Javier Garcia Vera <cf22javier.garcia@iesjoandaustria.org>
2023-04-05 16:59:45 +0200

    35_04

---
+++ 35_04_csv/Botiga.java
@@ -0,0 +1,87 @@
+/*
+ * Copyright 2022 Javier García Vera
+ * 
+ * This is free software, licensed under the GNU General Public License V3.
+ * See http://www.gnu.org/licenses/gpl.html for more information
+ * 
+ */
+//Aquest programa actua com una botiga de vins amb un espai maxim
+// i una quantitat de vins determinada
+import java.io.*;
+public class Botiga {
+    private final int DEFAULT_MAX_VINS=10;
+    private Vi[] vins; 
+
+
+
+    public Botiga(){
+        vins=new Vi[DEFAULT_MAX_VINS];
+    }
+
+    public Botiga(int maxVins){
+        if(maxVins<1){
+            vins=new Vi[DEFAULT_MAX_VINS];
+        }
+        else{
+            vins=new Vi[maxVins];
+        }
+    }
+
+    public Vi afegeix(Vi nouVi) throws IOException{
+        FileWriter file = new FileWriter("botiga.csv");
+        BufferedWriter botigacsv= new BufferedWriter(file);
+        if(nouVi.esValid()){
+            for(int i=0;i<vins.length;i++){
+                if(vins[i]!=null){
+                    if(nouVi.getNom().equals(vins[i].getNom())){
+                        return null;
+                    }
+                }
+            }
+            for(int i=0;i<vins.length;i++){
+                if(vins[i]==null){
+                    vins[i]=nouVi;
+                    return vins[i];
+                }
+            }
+        }
+        return null;
+    }
+
+    public Vi elimina(String aEliminar){
+        aEliminar=Vi.normalitzaNom(aEliminar);
+        for(int i=0;i<vins.length;i++){
+            if(vins[i]!=null){
+                if(vins[i].getNom().equals(aEliminar)){
+                    if(vins[i].getEstoc()<=0){
+                        Vi eliminat=vins[i];
+                        vins[i]=null;
+                        return eliminat;
+                    }
+                }
+            }     
+        }
+        return null;
+    }
+
+    public Vi cerca(String aTrobar){
+        aTrobar=Vi.normalitzaNom(aTrobar.toLowerCase());
+        for(int i=0;i<vins.length;i++){
+            if(vins[i]!=null){
+                if(vins[i].getNom().toLowerCase().equals(aTrobar)){
+                    return vins[i];
+                }
+            }
+        }
+        return null;
+    }
+
+    public void iniciaRecorregut() {
+
+    }
+
+    public Vi getSeguent(){
+        return new Vi("Default",0);
+    }
+}
+
---
+++ 35_04_csv/Vi.java
@@ -0,0 +1,105 @@
+/*
+ * Copyright 2022 Javier García Vera
+ * 
+ * This is free software, licensed under the GNU General Public License V3.
+ * See http://www.gnu.org/licenses/gpl.html for more information
+ * 
+ */
+//Aquest programa actua com un vi, amb nom preu i estoc
+public class Vi { 
+    private final String nom;
+    private int preu;
+    private int estoc;
+    public Vi(String preNom,int prePreu){
+        nom=normalitzaNom(preNom);
+        if(prePreu<0){
+            preu=-1;
+        }
+        else{
+            setPreu(prePreu);
+        }
+        setEstoc(0);
+    }
+    public Vi(String preNom,int prePreu,int prEstoc){
+        nom=normalitzaNom(preNom);
+        if(prePreu<0){
+            preu=-1;
+        }
+        else{
+            setPreu(prePreu);
+        }
+        if(prEstoc>=0){
+            setEstoc(prEstoc);
+        }else{ 
+            estoc=-1;
+        }
+    }
+    public static String normalitzaNom(String preNom){
+        if(preNom.isBlank()||preNom.isEmpty()){return "NOM NO VÀLID!";}
+        String nomFiltrat="";
+        preNom=preNom.trim();
+        for(int i=0;i<preNom.length();i++){
+            if(i>0&&Character.isWhitespace(preNom.charAt(i))&&Character.isWhitespace(preNom.charAt(i-1))){
+                continue;
+            }else{
+                nomFiltrat=nomFiltrat+preNom.charAt(i);
+            }
+        }
+        return nomFiltrat;
+    }
+    public void setEstoc(int prestoc){
+        if(prestoc>=0){
+            this.estoc=prestoc;
+        }
+    }
+    public void setPreu(int newPreu){
+        if (newPreu>=0){
+            this.preu=newPreu;
+        }
+    }
+    public String getNom(){return nom;}
+    public int getPreu(){return preu;}
+    public int getEstoc(){return estoc;}
+
+    public boolean esValid(){
+        if(nom.equals("NOM NO VÀLID!")||estoc<0||preu<0){
+            return false;
+        }
+        return true;
+    }
+    @Override public String toString(){
+        return String.format("""
+
+                Vi: %s
+                Preu: %d
+                Estoc: %d
+
+            """,nom,preu,estoc);
+    }
+
+    public String[] aArrayString(){
+        String[] valorsVi= new String[3];
+
+        valorsVi[0] = this.getNom();
+        valorsVi[1] = String.valueOf(this.getPreu());
+        valorsVi[2] = String.valueOf(this.getEstoc());
+        
+        return valorsVi;
+    }
+
+    public static Vi deArrayString(String[] vins){
+        String nomvi=vins[0];
+        String preuvi=vins[1];
+        String estocvi=vins[2];
+
+        if(UtilString.esEnter(preuvi)){
+            int preuint= Integer.parseInt(preuvi);
+            if(UtilString.esEnter(estocvi)){
+                int estocint= Integer.parseInt(estocvi);
+                nomvi=normalitzaNom(nomvi);
+                return new Vi(nomvi,preuint,estocint);
+            }
+        }
+        return null;
+    }
+}
