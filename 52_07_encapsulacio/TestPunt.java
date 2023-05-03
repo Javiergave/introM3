939794d5e34b125faf8afa675b19050296db47c0
prgtest <javiergv2004@gmail.com>
2023-05-03 18:17:38 +0200

    52_07  ∾ prgtest autocommit 52_07 #2

--- 52_07_encapsulacio/TestPunt.java
+++ 52_07_encapsulacio/TestPunt.java
@@ -48,9 +48,8 @@ public class TestPunt {
         // comprova que Punt() deixi a 0 la propietat x de Punt
 
         Punt p = new Punt();
-        Punt p1 = new Punt(0,0);
 
-        assertEquals(p,p1);
+        assertEquals(0, p.getX());
 
     }
 
