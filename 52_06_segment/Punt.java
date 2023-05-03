e094ca278a7198097a1c38806fd0eebf2cbcd495
Javier <javiergv2004@gmail.com>
2023-05-03 17:02:00 +0200

    52_06

---
+++ 52_06_segment/Punt.java
@@ -0,0 +1,41 @@
+public class Punt {
+
+    private int x = 0;
+    private int y = 0;
+
+    public int getX() {
+
+        return x;
+
+    }
+
+    public void suma(Punt p){ x+=p.x; y+=p.y; }
+
+    public void setX(int x) {
+
+        this.x = x;
+
+    }
+
+    public void setY(int y){
+        this.y=y;
+    }
+
+    public int getY(){ return y; }
+
+    public Punt(int x, int y) {
+
+        this.x = x;
+
+        this.y = y;
+
+    }
+
+    public Punt(){
+        this.x=0;
+        this.y=0;
+    }
+    public String toString(){
+        return ("x: "+x+", y: "+y);
+    }
+}
