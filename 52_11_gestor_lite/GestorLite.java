f5bdca65f6cddb5880b42d80159da39056674087
prgtest <javiergv2004@gmail.com>
2023-05-05 16:56:10 +0200

    52_11  ∾ prgtest autocommit 52_11 #8

--- 52_11_gestor_lite/GestorLite.java
+++ 52_11_gestor_lite/GestorLite.java
@@ -10,14 +10,16 @@
 public class GestorLite {
     public static void main(String[] args) {
         Client demo = creaClientDemo();
-        demo.setLloguers(new Lloguer(new Vehicle("Seat", "600", 1), 2));
-        demo.setLloguers(new Lloguer(new Vehicle("Tata", "Vista", 1), 5));
-        demo.setLloguers(new Lloguer(new Vehicle("Fiat", "Uno", 3), 2));
+        
         System.out.println(demo.getLloguers().size()); 
         mostraClient(demo);
     }
     public static Client creaClientDemo(){
-        return new Client("51590695Q","Eugènia Salinas Roig","93614214242");
+        Client c =new Client("51590695Q","Eugènia Salinas Roig","93614214242");
+        c.setLloguers(new Lloguer(new Vehicle("Seat", "600", 1), 2));
+        c.setLloguers(new Lloguer(new Vehicle("Tata", "Vista", 1), 5));
+        c.setLloguers(new Lloguer(new Vehicle("Fiat", "Uno", 3), 2));
+        return c;
     }  
     public static void mostraClient(Client c){
         System.out.println("Client: "+c.getNom());
