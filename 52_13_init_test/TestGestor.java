46f804a48e8a3e37386eb7059e20e12d696204f8
prgtest <javiergv2004@gmail.com>
2023-05-09 17:46:09 +0200

    52_13  ∾ prgtest autocommit 52_13 #22

--- 52_13_init_test/TestGestor.java
+++ 52_13_init_test/TestGestor.java
@@ -15,6 +15,6 @@ public class TestGestor {
     @Test
     public void informe(){
         Client c = GestorLite.creaClientDemo();
-        assertEquals(" ","Nif del client: "+c.getNif()+"\n"+"Nom del client: "+c.getNom()+"\n"+"Teléfon del client: "+c.getTelefon()+"\n"+"Llogers actius: "+c.getLloguers());
+        assertEquals(c.informe(),"Informe de lloguers del client Eugènia Salinas Roig (51590695Q)\n\tSeat 600: 90.0€\n\tTata Vista: 180.0€\n\tFiat Uno: 540.0€\nImport a pagar: 810.0€\nPunts guanyats: 4");
     }
 }
