7a9bdd6173062ebb24f809c4ab2014baac59b70e
Javier <javiergv2004@gmail.com>
2023-05-09 16:55:51 +0200

    52_13

---
+++ 52_13_init_test/Lloguer.java
@@ -0,0 +1,26 @@
+
+/*
+ * Copyright 2023 Javier García Vera
+ * 
+ * This is free software, licensed under the GNU General Public License V3.
+ * See http://www.gnu.org/licenses/gpl.html for more information
+ * 
+ */
+//Aquesta clase és un lloguer d'un vehicle que té un client
+public class Lloguer {
+    private int dies;
+    private Vehicle cotxe;
+    public Lloguer(int dia,Vehicle v){
+        dies=dia;
+        cotxe=v;
+    }
+    public Lloguer(Vehicle v,int dia){
+        dies=dia;
+        cotxe=v;
+    }
+
+    public Vehicle getVehicle(){ return cotxe; } 
+    public void setCosa(Vehicle c){  this.cotxe=c; } 
+    public int getDies(){ return dies;  }
+    public void setDies(int q){  dies = q;  }
+}
