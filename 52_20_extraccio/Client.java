ed1f5e59a17cc353ebe623afacda3b373497054f
Javier <javiergv2004@gmail.com>
2023-05-11 18:18:06 +0200

    52_20

---
+++ 52_20_extraccio/Client.java
@@ -0,0 +1,120 @@
+/*
+ * Copyright 2023 Javier García Vera
+ * 
+ * This is free software, licensed under the GNU General Public License V3.
+ * See http://www.gnu.org/licenses/gpl.html for more information
+ * 
+ */
+//Aquesta clase és un client que té una un nif, un nom, un teléfon i diferents lloguers
+
+import java.util.ArrayList;
+public class Client {
+
+    private String nif;
+
+    private String nom;
+
+    private String telefon;
+
+    private ArrayList<Lloguer> lloguers;
+
+
+    public Client(String nif, String nom, String telefon) {
+
+        this.nif = nif;
+
+        this.nom = nom;
+
+        this.telefon = telefon;
+
+        this.lloguers = new ArrayList<Lloguer>();
+
+    }
+
+
+    public String getNif()     { return nif; }
+
+    public String getNom()     { return nom; }
+
+    public String getTelefon() { return telefon; }
+
+    public ArrayList<Lloguer> getLloguers() { return lloguers; }
+
+    public void setLloguers(Lloguer l){
+        this.lloguers.add(l);
+    }
+
+    public void setNif(String nif) { this.nif = nif; }
+
+    public void setNom(String nom) { this.nom = nom; }
+
+    public void setTelefon(String telefon) { this.telefon = telefon; }
+
+
+    public String informe() {
+
+        double total = 0;
+    
+        int bonificacions = 0;
+    
+        String resultat = "Informe de lloguers del client " +
+    
+            getNom() +
+    
+            " (" + getNif() + ")\n";
+    
+        for (Lloguer lloguer: lloguers) {
+
+    
+    
+            // afegeix lloguers freqüents
+    
+            bonificacions +=bonificacionsDeLloguer(lloguer);
+    
+    
+            // afegeix bonificació per dos dies de lloguer de Luxe
+    
+            
+    
+    
+            // composa els resultats d'aquest lloguer
+    
+            resultat += "\t" +
+    
+                lloguer.getVehicle().getMarca() +
+    
+                " " +
+    
+                lloguer.getVehicle().getModel() + ": " +
+    
+                (lloguer.quantitatPerLloguer()* 30) + "€" + "\n";
+    
+            total += lloguer.quantitatPerLloguer() * 30;
+    
+        }
+    
+    
+        // afegeix informació final
+    
+        resultat += "Import a pagar: " + total + "€\n" +
+    
+            "Punts guanyats: " + bonificacions + "\n";
+    
+        return resultat;
+    
+    }
+
+    public int bonificacionsDeLloguer(Lloguer l){
+        if (l.getVehicle().getCategoria() == Vehicle.LUXE &&
+    
+                    l.getDies()>1 ) {
+    
+                return 2;
+    
+        }else{
+            return 1;
+        }
+    }
+
+    
+}
