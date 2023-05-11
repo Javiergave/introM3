cbea8f6b3a0de9c19c35fb452307ecf46d23c233
Javier <javiergv2004@gmail.com>
2023-05-11 18:32:45 +0200

    52_24

---
+++ 52_24_elimina_bonificacions/Client.java
@@ -0,0 +1,105 @@
+/*
+ * Copyright 2023 Javier García Vera
+ * 
+ * This is free software, licensed under the GNU General Public License V3.
+ * See http://www.gnu.org/licenses/gpl.html for more information
+ * 
+ */
+//Aquesta clase és un client que té una un nif, un nom, un teléfon i diferents lloguers
+
+import java.util.ArrayList;
+public class Client {
+
+    private String nif;
+
+    private String nom;
+
+    private String telefon;
+
+    private ArrayList<Lloguer> lloguers;
+
+
+    public Client(String nif, String nom, String telefon) {
+
+        this.nif = nif;
+
+        this.nom = nom;
+
+        this.telefon = telefon;
+
+        this.lloguers = new ArrayList<Lloguer>();
+
+    }
+
+
+    public String getNif()     { return nif; }
+
+    public String getNom()     { return nom; }
+
+    public String getTelefon() { return telefon; }
+
+    public ArrayList<Lloguer> getLloguers() { return lloguers; }
+
+    public void setLloguers(Lloguer l){
+        this.lloguers.add(l);
+    }
+
+    public void setNif(String nif) { this.nif = nif; }
+
+    public void setNom(String nom) { this.nom = nom; }
+
+    public void setTelefon(String telefon) { this.telefon = telefon; }
+
+
+    public String informe() {
+    
+        String resultat = "Informe de lloguers del client " +
+    
+            getNom() +
+    
+            " (" + getNif() + ")\n";
+    
+        for (Lloguer lloguer: lloguers) {
+            // composa els resultats d'aquest lloguer
+    
+            resultat += "\t" +
+    
+                lloguer.getVehicle().getMarca() +
+    
+                " " +
+    
+                lloguer.getVehicle().getModel() + ": " +
+    
+                (lloguer.quantitatPerLloguer()* 30) + "€" + "\n";
+    
+    
+        }
+    
+    
+        // afegeix informació final
+    
+        resultat += "Import a pagar: " + importTotal() + "€\n" +
+    
+            "Punts guanyats: " + bonificacionsTotal() + "\n";
+    
+        return resultat;
+    
+    }
+
+    private double importTotal(){
+        double total=0;
+        for (Lloguer lloguer: lloguers) {
+            total+=lloguer.quantitatPerLloguer() * 30;
+        }
+        return total;
+
+    }  
+    private int bonificacionsTotal(){
+        int total=0;
+        for (Lloguer lloguer: lloguers) {
+            total+=lloguer.bonificacionsDeLloguer();
+        }
+        return total;
+
+    }   
+}
---
+++ 52_24_elimina_bonificacions/Lloguer.java
@@ -0,0 +1,80 @@
+
+/*
+ * Copyright 2023 Javier García Vera
+ * 
+ * This is free software, licensed under the GNU General Public License V3.
+ * See http://www.gnu.org/licenses/gpl.html for more information
+ * 
+ */
+//Aquesta clase és un lloguer d'un vehicle que té un client
+public class Lloguer {
+    private int dies;
+    private Vehicle cotxe;
+    public Lloguer(int dia,Vehicle v){
+        dies=dia;
+        cotxe=v;
+    }
+    public Lloguer(Vehicle v,int dia){
+        dies=dia;
+        cotxe=v;
+    }
+
+    public Vehicle getVehicle(){ return cotxe; } 
+    public void setCosa(Vehicle c){  this.cotxe=c; } 
+    public int getDies(){ return dies;  }
+    public void setDies(int q){  dies = q;  }
+    @Override
+    public String toString(){
+        return ("\t"+cotxe.getMarca()+" "+cotxe.getModel()+": "+(cotxe.getCategoria()*90)+".0€");
+    }
+    public double quantitatPerLloguer(){
+        double quantitat = 0;
+    
+        switch (this.getVehicle().getCategoria()) {
+    
+            case 1:
+    
+                quantitat += 3;
+    
+                if (this.getDies() > 3) {
+  
+                    quantitat += (this.getDies() - 3) * 1.5;
+   
+                }
+    
+                break;
+    
+            case 2:
+    
+                quantitat += 4;
+    
+                if (this.getDies() > 2) {
+    
+                    quantitat += (this.getDies() - 2) * 2.5;
+    
+                }
+    
+                break;
+    
+            case 3:
+    
+                quantitat += this.getDies() * 6;
+    
+                break;
+    
+        }
+        return quantitat;
+    }
+
+    public int bonificacionsDeLloguer(){
+        if (this.getVehicle().getCategoria() == Vehicle.LUXE &&
+    
+                    this.getDies()>1 ) {
+    
+                return 2;
+    
+        }else{
+            return 1;
+        }
+    }
+}
