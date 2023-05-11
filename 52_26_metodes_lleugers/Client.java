6e237f1dffa44b4c9bd790a475f0f06db1c0e3f2
Javier <javiergv2004@gmail.com>
2023-05-11 18:41:03 +0200

    52_24

---
+++ 52_26_metodes_lleugers/Client.java
@@ -0,0 +1,107 @@
+/*
+ * Copyright 2023 Javier García Vera
+ * 
+ * This is free software, licensed under the GNU General Public License V3.
+ * See http://www.gnu.org/licenses/gpl.html for more information
+ * 
+ */
+//Aquesta clase és un client que té una un nif, un nom, un teléfon i diferents lloguers
+
+import java.util.ArrayList;
+public class Client {
+
+    private String nif;
+
+    private String nom;
+
+    private String telefon;
+
+    private ArrayList<Lloguer> lloguers;
+
+
+    public Client(String nif, String nom, String telefon) {
+
+        this.nif = nif;
+
+        this.nom = nom;
+
+        this.telefon = telefon;
+
+        this.lloguers = new ArrayList<Lloguer>();
+
+    }
+
+
+    public String getNif()     { return nif; }
+
+    public String getNom()     { return nom; }
+
+    public String getTelefon() { return telefon; }
+
+    public ArrayList<Lloguer> getLloguers() { return lloguers; }
+
+    public void setLloguers(Lloguer l){
+        this.lloguers.add(l);
+    }
+
+    public void setNif(String nif) { this.nif = nif; }
+
+    public void setNom(String nom) { this.nom = nom; }
+
+    public void setTelefon(String telefon) { this.telefon = telefon; }
+
+
+    public String informe() {
+    
+       return composaCapsalera()+composaDetall()+composaPeu();
+    
+    }
+
+    private double importTotal(){
+        double total=0;
+        for (Lloguer lloguer: lloguers) {
+            total+=lloguer.quantitatPerLloguer() * 30;
+        }
+        return total;
+
+    }  
+    private int bonificacionsTotal(){
+        int total=0;
+        for (Lloguer lloguer: lloguers) {
+            total+=lloguer.bonificacionsDeLloguer();
+        }
+        return total;
+
+    }   
+    private String composaCapsalera(){
+        return "Informe de lloguers del client " +
+    
+        getNom() +
+
+        " (" + getNif() + ")\n";
+    }
+    private String composaDetall(){
+        String resultat=""; 
+        for (Lloguer lloguer: lloguers) {
+            // composa els resultats d'aquest lloguer
+    
+            resultat += "\t" +
+    
+                lloguer.getVehicle().getMarca() +
+    
+                " " +
+    
+                lloguer.getVehicle().getModel() + ": " +
+    
+                (lloguer.quantitatPerLloguer()* 30) + "€" + "\n";
+        }
+        return resultat;
+    }
+    private String composaPeu(){
+        return "Import a pagar: " + importTotal() + "€\n" +
+    
+        "Punts guanyats: " + bonificacionsTotal() + "\n";
+    
+    }
+
+}
