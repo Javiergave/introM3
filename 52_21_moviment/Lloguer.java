42987e1a12c629ed346aac06bd85d8e9d85fb730
Javier <javiergv2004@gmail.com>
2023-05-17 18:50:24 +0200

    fix 21

--- 52_21_moviment/Lloguer.java
+++ 52_21_moviment/Lloguer.java
@@ -27,7 +27,7 @@ public class Lloguer {
     public String toString(){
         return ("\t"+cotxe.getMarca()+" "+cotxe.getModel()+": "+(cotxe.getCategoria()*90)+".0€");
     }
-    public double quantitatPerLloguer(){
+    public double quantitat(){
         double quantitat = 0;
     
         switch (this.getVehicle().getCategoria()) {
@@ -66,7 +66,7 @@ public class Lloguer {
         return quantitat;
     }
 
-    public int bonificacionsDeLloguer(){
+    public int bonificacions(){
         if (this.getVehicle().getCategoria() == Vehicle.LUXE &&
     
                     this.getDies()>1 ) {
